d7287cb738dde7bc7a8b64ef6a74977ad4609f7b
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-18 15:20:26 +0000

    Add currency formatting utility with locale support

---
+++ src/currencyFormatter.js
@@ -0,0 +1,21 @@
+function formatCurrency(amount, locale = 'en-US', currency = 'USD') {
+    return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: currency
+    }).format(amount);
+}
+
+function parseCurrency(formattedString, locale = 'en-US') {
+    const parts = new Intl.NumberFormat(locale).formatToParts(1234.5);
+    const decimalSeparator = parts.find(part => part.type === 'decimal').value;
+    const groupSeparator = parts.find(part => part.type === 'group').value;
+    
+    const cleaned = formattedString
+        .replace(new RegExp(`\\${groupSeparator}`, 'g'), '')
+        .replace(new RegExp(`\\${decimalSeparator}`), '.')
+        .replace(/[^\d.-]/g, '');
+    
+    return parseFloat(cleaned);
+}
+
+export { formatCurrency, parseCurrency };
