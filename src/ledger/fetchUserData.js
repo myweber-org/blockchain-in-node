8023b4087f9c310ed4f52ad5c20e2de9e7672c71
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-19 14:11:07 +0000

    Add fetchUserData function to retrieve and process user data from API

--- src/ledger/fetchUserData.js
+++ src/ledger/fetchUserData.js
@@ -59,4 +59,19 @@ async function getUserProfile(userId) {
     }
 }
 
-export { fetchUserData, getUserProfile };
+export { fetchUserData, getUserProfile };function fetchUserData(userId) {
+  return fetch(`https://api.example.com/users/${userId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('User data fetched successfully:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
