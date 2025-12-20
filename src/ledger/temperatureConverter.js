7b5447d2194a7a754a442f45f91d82ed58e6e3ee
InkspillVerse <inkspill.verse@outlook.com>
2025-12-20 20:40:18 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and Kelvin functions

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -20,4 +20,60 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function celsiusToKelvin(celsius) {
+    return celsius + 273.15;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function fahrenheitToKelvin(fahrenheit) {
+    return celsiusToKelvin(fahrenheitToCelsius(fahrenheit));
+}
+
+function kelvinToCelsius(kelvin) {
+    return kelvin - 273.15;
+}
+
+function kelvinToFahrenheit(kelvin) {
+    return celsiusToFahrenheit(kelvinToCelsius(kelvin));
+}
+
+function convertTemperature(value, fromUnit, toUnit) {
+    const units = {
+        'C': 'celsius',
+        'F': 'fahrenheit', 
+        'K': 'kelvin'
+    };
+    
+    if (!units[fromUnit] || !units[toUnit]) {
+        throw new Error('Invalid temperature unit');
+    }
+    
+    if (fromUnit === toUnit) {
+        return value;
+    }
+    
+    const conversionFunction = `${units[fromUnit]}To${units[toUnit].charAt(0).toUpperCase() + units[toUnit].slice(1)}`;
+    
+    if (typeof window[conversionFunction] === 'function') {
+        return window[conversionFunction](value);
+    }
+    
+    throw new Error(`Conversion from ${fromUnit} to ${toUnit} not supported`);
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    celsiusToKelvin,
+    fahrenheitToCelsius,
+    fahrenheitToKelvin,
+    kelvinToCelsius,
+    kelvinToFahrenheit,
+    convertTemperature
 };
