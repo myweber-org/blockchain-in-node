b994f7172b262d5cec3c52c95387d6b7879c48c8
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-21 20:58:07 +0000

    Implement a function to compute the nth Fibonacci number using memoization for optimization.

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -22,4 +22,11 @@ function isFibonacciNumber(num) {
     return b === num;
 }
 
-module.exports = { calculateFibonacci, isFibonacciNumber };
+module.exports = { calculateFibonacci, isFibonacciNumber };function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
+}
+
+console.log(fibonacci(10));
