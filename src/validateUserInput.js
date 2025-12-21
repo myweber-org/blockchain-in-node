76944c29975a2bb14a02f4c3b6430204edd9e4e2
InkspillVerse <inkspill.verse@outlook.com>
2025-12-21 15:27:04 +0000

    Add input validation function to sanitize and check user-provided strings

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -517,4 +517,26 @@ module.exports = {
     sanitizeInput,
     validateEmail,
     validatePassword
-};
+};function validateUserInput(input) {
+    if (typeof input !== 'string') {
+        throw new TypeError('Input must be a string');
+    }
+    
+    const trimmedInput = input.trim();
+    
+    if (trimmedInput.length === 0) {
+        throw new Error('Input cannot be empty or whitespace only');
+    }
+    
+    const maxLength = 255;
+    if (trimmedInput.length > maxLength) {
+        throw new Error(`Input exceeds maximum length of ${maxLength} characters`);
+    }
+    
+    const dangerousPatterns = /[<>{}[\]\\]/;
+    if (dangerousPatterns.test(trimmedInput)) {
+        throw new Error('Input contains potentially dangerous characters');
+    }
+    
+    return trimmedInput;
+}
