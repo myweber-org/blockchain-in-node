a1cf90141a02bb55973b22a0431f30f6d179594a
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-23 16:23:17 +0000

    Add utility function for secure password hashing using bcrypt

---
+++ src/passwordHasher.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = async (password, saltRounds = 10) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error('Password hashing failed');
+  }
+};
+
+const comparePassword = async (password, hashedPassword) => {
+  try {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    return isMatch;
+  } catch (error) {
+    throw new Error('Password comparison failed');
+  }
+};
+
+module.exports = { hashPassword, comparePassword };
