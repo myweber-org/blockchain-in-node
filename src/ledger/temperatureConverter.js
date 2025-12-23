4144ccc3759af3c88ea8514f7139589d7a8be83e
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-23 15:40:50 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -136,4 +136,36 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+    if (unit.toLowerCase() === 'c') {
+        return {
+            value: celsiusToFahrenheit(value),
+            unit: 'F'
+        };
+    } else if (unit.toLowerCase() === 'f') {
+        return {
+            value: fahrenheitToCelsius(value),
+            unit: 'C'
+        };
+    }
+    throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
+}
+
+function formatTemperature(value, unit) {
+    return `${value.toFixed(1)}°${unit}`;
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius,
+    convertTemperature,
+    formatTemperature
 };
