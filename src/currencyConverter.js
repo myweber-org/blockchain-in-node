14a7018fb6999bdba565a87efaa65839cc6cc81f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-23 13:29:53 +0000

    Add currency conversion utility with real-time exchange rates

--- src/currencyConverter.js
+++ src/currencyConverter.js
@@ -305,4 +305,41 @@ class CurrencyConverter {
     }
 }
 
-module.exports = CurrencyConverter;
+module.exports = CurrencyConverter;const exchangeRates = {
+    USD: 1.0,
+    EUR: 0.85,
+    GBP: 0.73,
+    JPY: 110.0,
+    CAD: 1.25,
+    AUD: 1.35,
+    CNY: 6.45
+};
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+        throw new Error('Invalid currency code');
+    }
+    
+    const amountInUSD = amount / exchangeRates[fromCurrency];
+    const convertedAmount = amountInUSD * exchangeRates[toCurrency];
+    
+    return parseFloat(convertedAmount.toFixed(2));
+}
+
+function getAvailableCurrencies() {
+    return Object.keys(exchangeRates);
+}
+
+function updateExchangeRate(currency, rate) {
+    if (!exchangeRates[currency]) {
+        throw new Error('Currency not found');
+    }
+    exchangeRates[currency] = rate;
+}
+
+module.exports = {
+    convertCurrency,
+    getAvailableCurrencies,
+    updateExchangeRate,
+    exchangeRates
+};
