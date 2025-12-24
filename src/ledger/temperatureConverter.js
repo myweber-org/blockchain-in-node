069c4da48b2ca25793618620664c0215f7e167ae
InkspillVerse <inkspill.verse@outlook.com>
2025-12-24 14:22:59 +0000

    Add temperature conversion utility with Celsius to Fahrenheit function

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -190,4 +190,28 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, fromUnit, toUnit) {
+    if (fromUnit === 'C' && toUnit === 'F') {
+        return celsiusToFahrenheit(value);
+    } else if (fromUnit === 'F' && toUnit === 'C') {
+        return fahrenheitToCelsius(value);
+    } else if (fromUnit === toUnit) {
+        return value;
+    } else {
+        throw new Error('Unsupported temperature conversion');
+    }
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius,
+    convertTemperature
 };
