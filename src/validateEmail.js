b21510e0c02505e65d89b2288473dca46bd6705a
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-24 15:04:56 +0000

    Add email validation utility with regex pattern

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -75,4 +75,9 @@ module.exports = validateEmail;function validateEmail(email) {
     return emailRegex.test(email);
 }
 
+module.exports = validateEmail;function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 module.exports = validateEmail;
