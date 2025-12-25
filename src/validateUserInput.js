b2f16dd50f3b36024e2c050bc495e4a0aaccf4d6
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-25 00:30:52 +0000

    Add input validation function for username and password

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -75,4 +75,29 @@ module.exports = {
     sanitizeInput,
     validateEmail,
     validatePassword
-};
+};function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const isUsernameValid = validateUsername(username);
+    const isPasswordValid = validatePassword(password);
+    
+    if (!isUsernameValid) {
+        return { valid: false, message: 'Username must be 3-20 characters and contain only letters, numbers, and underscores.' };
+    }
+    
+    if (!isPasswordValid) {
+        return { valid: false, message: 'Password must be at least 8 characters and contain at least one letter and one number.' };
+    }
+    
+    return { valid: true, message: 'Input validation successful.' };
+}
+
+export { validateUserInput, validateUsername, validatePassword };
