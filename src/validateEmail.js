cea34debb774148d428b904f155396c44abf5c4a
InkspillVerse <inkspill.verse@outlook.com>
2025-12-25 22:56:20 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -6,4 +6,9 @@ function validateEmail(email) {
 module.exports = validateEmail;function validateEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
+}function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
+
+module.exports = validateEmail;
