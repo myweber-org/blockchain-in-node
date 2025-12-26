3c70166f336cb930b2c2b64cb06b5ca2aeb29242
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-26 23:30:07 +0000

    Add password strength validation utility with regex patterns

--- src/ledger/passwordValidator.js
+++ src/ledger/passwordValidator.js
@@ -120,4 +120,32 @@ function calculatePasswordScore(password, config) {
     return Math.min(Math.max(score, 0), 100);
 }
 
-module.exports = { validatePassword, calculatePasswordScore };
+module.exports = { validatePassword, calculatePasswordScore };function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    if (password.length < minLength) {
+        return { isValid: false, message: 'Password must be at least 8 characters long' };
+    }
+    
+    if (!hasUpperCase) {
+        return { isValid: false, message: 'Password must contain at least one uppercase letter' };
+    }
+    
+    if (!hasLowerCase) {
+        return { isValid: false, message: 'Password must contain at least one lowercase letter' };
+    }
+    
+    if (!hasNumbers) {
+        return { isValid: false, message: 'Password must contain at least one number' };
+    }
+    
+    if (!hasSpecialChar) {
+        return { isValid: false, message: 'Password must contain at least one special character' };
+    }
+    
+    return { isValid: true, message: 'Password meets all security requirements' };
+}
