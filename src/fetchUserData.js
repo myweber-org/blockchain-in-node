c7d6339d9a5c13e091520a258f052c5ba4a915cf
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-28 20:31:30 +0000

    Add fetchUserData function to retrieve and log user data from API

--- src/fetchUserData.js
+++ src/fetchUserData.js
@@ -118,4 +118,21 @@ function validateUserId(userId) {
     return typeof userId === 'string' && userId.length > 0;
 }
 
-export { fetchUserData, validateUserId };
+export { fetchUserData, validateUserId };function fetchUserData(userId) {
+    const apiUrl = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('User Data:', data);
+            return data;
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
+}
