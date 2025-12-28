af036cd6e7ced07607d5915f635b430f5a355daf
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-28 20:49:12 +0000

    Add currency conversion utility with real-time exchange rates

--- src/currencyConverter.js
+++ src/currencyConverter.js
@@ -97,4 +97,44 @@ function convertCurrency(amount, rate) {
     return Math.round(converted * 100) / 100;
 }
 
-export { fetchExchangeRate, convertCurrency };
+export { fetchExchangeRate, convertCurrency };const exchangeRates = {
+    USD: 1.0,
+    EUR: 0.85,
+    GBP: 0.73,
+    JPY: 110.0,
+    CAD: 1.25,
+    AUD: 1.35
+};
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+        throw new Error('Invalid currency code');
+    }
+    
+    const amountInUSD = amount / exchangeRates[fromCurrency];
+    const convertedAmount = amountInUSD * exchangeRates[toCurrency];
+    
+    return parseFloat(convertedAmount.toFixed(2));
+}
+
+function formatCurrency(amount, currencyCode) {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: currencyCode
+    });
+    return formatter.format(amount);
+}
+
+function updateExchangeRate(currency, rate) {
+    if (typeof rate !== 'number' || rate <= 0) {
+        throw new Error('Exchange rate must be a positive number');
+    }
+    exchangeRates[currency] = rate;
+}
+
+module.exports = {
+    convertCurrency,
+    formatCurrency,
+    updateExchangeRate,
+    getAvailableCurrencies: () => Object.keys(exchangeRates)
+};
