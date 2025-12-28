a9b328cf3f40800a5783da17668cc1da962a90d5
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-28 15:37:44 +0000

    Add utility function to convert Celsius to Fahrenheit

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -90,4 +90,15 @@ function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5/9;
 }
 
-export { celsiusToFahrenheit, fahrenheitToCelsius };
+export { celsiusToFahrenheit, fahrenheitToCelsius };function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius
+};
