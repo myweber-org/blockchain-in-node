0761ea417d58146057611d7deafafd4385429398
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-30 22:58:23 +0000

    Add email validation utility function

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -27,4 +27,9 @@ if (typeof module !== 'undefined' && module.exports) {
 }function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
+}function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 }
+
+module.exports = validateEmail;
