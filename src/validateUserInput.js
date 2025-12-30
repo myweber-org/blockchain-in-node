6214c48e137a4c9776eddab4872f752cb16aceb3
InkspillVerse <inkspill.verse@outlook.com>
2025-12-30 23:01:39 +0000

    Add input validation function to sanitize user-provided strings

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -49,4 +49,40 @@ export { validateUserInput, validateUsername, validatePassword };function valida
     }
 
     return true;
+}function sanitizeInput(input) {
+  if (typeof input !== 'string') {
+    return '';
+  }
+  
+  const trimmed = input.trim();
+  const escaped = trimmed
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#x27;')
+    .replace(/\//g, '&#x2F;');
+    
+  return escaped;
 }
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  const minLength = 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  
+  return password.length >= minLength && 
+         hasUpperCase && 
+         hasLowerCase && 
+         hasNumbers && 
+         hasSpecialChar;
+}
+
+export { sanitizeInput, validateEmail, validatePassword };
