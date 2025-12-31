dae067785507cecb7fe26980f1bdf2e2f1f81e36
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-31 20:11:22 +0000

    Add email validation utility with regex pattern

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -37,4 +37,9 @@ module.exports = validateEmail;function validateEmail(email) {
     return regex.test(email);
 }
 
+module.exports = validateEmail;function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 module.exports = validateEmail;
