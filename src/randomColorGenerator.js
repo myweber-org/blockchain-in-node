d67cdfd00e08a6dca67e5c1420375dec0b4f08a1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-31 23:50:48 +0000

    Add random color generator utility function

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -1,4 +1,3 @@
-
 function generateRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -7,43 +6,3 @@ function generateRandomColor() {
     }
     return color;
 }
-
-function generateRandomRgbColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-}
-
-function generateRandomHslColor() {
-    const h = Math.floor(Math.random() * 360);
-    const s = Math.floor(Math.random() * 101);
-    const l = Math.floor(Math.random() * 101);
-    return `hsl(${h}, ${s}%, ${l}%)`;
-}
-
-export { generateRandomColor, generateRandomRgbColor, generateRandomHslColor };function generateRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function generateRandomRGBA() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    const a = Math.random().toFixed(2);
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
-
-function generateRandomHSL() {
-    const h = Math.floor(Math.random() * 360);
-    const s = Math.floor(Math.random() * 101);
-    const l = Math.floor(Math.random() * 101);
-    return `hsl(${h}, ${s}%, ${l}%)`;
-}
-
-export { generateRandomColor, generateRandomRGBA, generateRandomHSL };
