96821639da2f5c2cf7fd522b1b692d82cbe87150
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-01 19:31:43 +0000

    Add function to generate Fibonacci sequence up to n terms

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -2,44 +2,9 @@ function calculateFibonacci(n) {
     if (n <= 0) return [];
     if (n === 1) return [0];
     
-    let fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-        fib.push(fib[i-1] + fib[i-2]);
-    }
-    return fib;
-}function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    
-    const sequence = [0, 1];
+    let sequence = [0, 1];
     for (let i = 2; i < n; i++) {
         sequence.push(sequence[i - 1] + sequence[i - 2]);
     }
     return sequence;
 }
-
-function validateInput(input) {
-    const num = parseInt(input);
-    if (isNaN(num) || num < 0) {
-        throw new Error('Input must be a non-negative integer');
-    }
-    return num;
-}
-
-function displayFibonacciSequence(input) {
-    try {
-        const n = validateInput(input);
-        const result = calculateFibonacci(n);
-        console.log(`Fibonacci sequence (${n} terms): ${result.join(', ')}`);
-        return result;
-    } catch (error) {
-        console.error('Error:', error.message);
-        return null;
-    }
-}
-
-module.exports = {
-    calculateFibonacci,
-    validateInput,
-    displayFibonacciSequence
-};
