b3e4f00ca5775db1119ba6a13ca0004b83a9aa1c
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-02 16:49:08 +0000

    Add password strength validation utility with configurable rules

--- src/passwordValidator.js
+++ src/passwordValidator.js
@@ -112,4 +112,67 @@ function calculatePasswordScore(password, config) {
     return Math.min(100, score);
 }
 
-export default passwordValidator;
+export default passwordValidator;function validatePassword(password, options = {}) {
+    const defaults = {
+        minLength: 8,
+        requireUppercase: true,
+        requireLowercase: true,
+        requireNumbers: true,
+        requireSpecialChars: true,
+        specialChars: "!@#$%^&*()_+-=[]{}|;:,.<>?"
+    };
+    
+    const config = { ...defaults, ...options };
+    const errors = [];
+    
+    if (password.length < config.minLength) {
+        errors.push(`Password must be at least ${config.minLength} characters long`);
+    }
+    
+    if (config.requireUppercase && !/[A-Z]/.test(password)) {
+        errors.push("Password must contain at least one uppercase letter");
+    }
+    
+    if (config.requireLowercase && !/[a-z]/.test(password)) {
+        errors.push("Password must contain at least one lowercase letter");
+    }
+    
+    if (config.requireNumbers && !/\d/.test(password)) {
+        errors.push("Password must contain at least one number");
+    }
+    
+    if (config.requireSpecialChars) {
+        const specialCharRegex = new RegExp(`[${config.specialChars.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}]`);
+        if (!specialCharRegex.test(password)) {
+            errors.push("Password must contain at least one special character");
+        }
+    }
+    
+    return {
+        isValid: errors.length === 0,
+        errors: errors,
+        score: calculatePasswordScore(password, config)
+    };
+}
+
+function calculatePasswordScore(password, config) {
+    let score = 0;
+    
+    if (password.length >= config.minLength) score += 25;
+    if (password.length >= config.minLength + 4) score += 15;
+    
+    if (/[A-Z]/.test(password)) score += 15;
+    if (/[a-z]/.test(password)) score += 15;
+    if (/\d/.test(password)) score += 15;
+    
+    const specialCharRegex = new RegExp(`[${config.specialChars.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}]`);
+    if (specialCharRegex.test(password)) score += 15;
+    
+    if (/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{12,}$/.test(password)) {
+        score += 10;
+    }
+    
+    return Math.min(score, 100);
+}
+
+export { validatePassword, calculatePasswordScore };
