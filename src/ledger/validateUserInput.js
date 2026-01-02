712f5ab1461cd45111eda59ab2309bc27e1aca5a
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-02 22:27:53 +0000

    Add input validation function to sanitize user-provided strings

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -20,4 +20,40 @@ function validateUserInput(username, password) {
         isValid: true,
         message: 'Input validation passed.'
     };
+}function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;');
 }
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
