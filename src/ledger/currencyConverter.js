4b939c1c6b52dce7053234687e4939adb23471dd
InkspillVerse <inkspill.verse@outlook.com>
2026-01-03 23:16:26 +0000

    Add currency conversion utility with exchange rate caching

--- src/ledger/currencyConverter.js
+++ src/ledger/currencyConverter.js
@@ -34,4 +34,37 @@ module.exports = {
     convertCurrency,
     getAvailableCurrencies,
     updateExchangeRate
+};const exchangeRates = {
+  USD: 1.0,
+  EUR: 0.85,
+  GBP: 0.73,
+  JPY: 110.0,
+  CAD: 1.25
 };
+
+const cache = new Map();
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+  const cacheKey = `${fromCurrency}_${toCurrency}`;
+  
+  if (!cache.has(cacheKey)) {
+    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+      throw new Error('Unsupported currency');
+    }
+    const rate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
+    cache.set(cacheKey, rate);
+  }
+  
+  const rate = cache.get(cacheKey);
+  return amount * rate;
+}
+
+function formatCurrency(amount, currency) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency
+  });
+  return formatter.format(amount);
+}
+
+export { convertCurrency, formatCurrency };
