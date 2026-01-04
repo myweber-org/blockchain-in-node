66a7798548ef783e05eef52d703b8ed2d39342ac
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-04 23:11:22 +0000

    Add data validation utility for user input sanitization

--- src/dataValidator.js
+++ src/dataValidator.js
@@ -338,4 +338,41 @@ function validateFormData(formData) {
     };
 }
 
-export { validateEmail, validatePassword, sanitizeInput, validateFormData };
+export { validateEmail, validatePassword, sanitizeInput, validateFormData };function sanitizeInput(input) {
+  if (typeof input !== 'string') {
+    return '';
+  }
+  
+  const trimmed = input.trim();
+  const sanitized = trimmed.replace(/[<>]/g, '');
+  
+  if (sanitized.length > 100) {
+    return sanitized.substring(0, 100);
+  }
+  
+  return sanitized;
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false;
+  }
+  
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  
+  return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+module.exports = {
+  sanitizeInput,
+  validateEmail,
+  validatePassword
+};
