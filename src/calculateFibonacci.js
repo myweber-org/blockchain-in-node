463d4df7439f9bf2335d0bce718792ada4e479d2
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-04 16:34:07 +0000

    Implement a function to compute the nth Fibonacci number using memoization for efficiency.

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -58,4 +58,11 @@ function displayFibonacciResult(inputElement, outputElement) {
     } catch (error) {
         outputElement.textContent = 'Error: ' + error.message;
     }
+}function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
+
+console.log(fibonacci(10));
