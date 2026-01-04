ef2c8f34e1be849ac7421b4293c3a88b4a8d2471
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-04 22:39:09 +0000

    Add user preferences management with local storage support

--- src/userPreferencesManager.js
+++ src/userPreferencesManager.js
@@ -48,4 +48,38 @@ const UserPreferencesManager = (function() {
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = UserPreferencesManager;
-}
+}const UserPreferencesManager = {
+    preferences: {},
+
+    init: function() {
+        const stored = localStorage.getItem('userPreferences');
+        if (stored) {
+            this.preferences = JSON.parse(stored);
+        }
+    },
+
+    setPreference: function(key, value) {
+        this.preferences[key] = value;
+        this.save();
+    },
+
+    getPreference: function(key, defaultValue = null) {
+        return this.preferences[key] || defaultValue;
+    },
+
+    removePreference: function(key) {
+        delete this.preferences[key];
+        this.save();
+    },
+
+    save: function() {
+        localStorage.setItem('userPreferences', JSON.stringify(this.preferences));
+    },
+
+    clearAll: function() {
+        this.preferences = {};
+        localStorage.removeItem('userPreferences');
+    }
+};
+
+UserPreferencesManager.init();
