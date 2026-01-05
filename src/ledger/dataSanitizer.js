62b5ca85aa4cf14873f8ad2189ed4916f412af92
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-05 14:17:42 +0000

    Add data sanitization function to prevent XSS attacks

--- src/ledger/dataSanitizer.js
+++ src/ledger/dataSanitizer.js
@@ -42,4 +42,30 @@ function validateAndSanitize(userInput) {
     return sanitized.length > maxLength ? sanitized.substring(0, maxLength) : sanitized;
 }
 
-module.exports = { validateAndSanitize };
+module.exports = { validateAndSanitize };function sanitizeInput(input) {
+    const div = document.createElement('div');
+    div.textContent = input;
+    return div.innerHTML;
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function escapeHtml(text) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    escapeHtml
+};
