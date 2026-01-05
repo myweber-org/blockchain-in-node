6a691f72928f593139534111977ca79d685e6651
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-05 20:09:57 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -48,4 +48,33 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+  if (unit.toLowerCase() === 'c') {
+    return {
+      celsius: value,
+      fahrenheit: celsiusToFahrenheit(value),
+      unit: 'C'
+    };
+  } else if (unit.toLowerCase() === 'f') {
+    return {
+      celsius: fahrenheitToCelsius(value),
+      fahrenheit: value,
+      unit: 'F'
+    };
+  }
+  throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
+}
+
+module.exports = {
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+  convertTemperature
 };
