320d32e7437677336501f1c2e9dbfe25ff76b05a
InkspillVerse <inkspill.verse@outlook.com>
2026-01-06 21:02:46 +0000

    Add input sanitization function to prevent XSS attacks

--- src/ledger/dataSanitizer.js
+++ src/ledger/dataSanitizer.js
@@ -99,4 +99,33 @@ function processUserData(userInput) {
     };
 }
 
-module.exports = { sanitizeInput, validateEmail, processUserData };
+module.exports = { sanitizeInput, validateEmail, processUserData };function sanitizeInput(input) {
+    const div = document.createElement('div');
+    div.textContent = input;
+    return div.innerHTML;
+}
+
+function validateAndSanitize(userInput) {
+    if (typeof userInput !== 'string') {
+        return '';
+    }
+    
+    const trimmedInput = userInput.trim();
+    if (trimmedInput.length === 0) {
+        return '';
+    }
+    
+    const sanitized = sanitizeInput(trimmedInput);
+    const dangerousPatterns = /<script|javascript:|on\w+\s*=/i;
+    
+    if (dangerousPatterns.test(sanitized)) {
+        console.warn('Potentially dangerous input detected and neutralized');
+        return sanitized.replace(dangerousPatterns, match => {
+            return match.replace(/./g, '&#x' + match.charCodeAt(0).toString(16) + ';');
+        });
+    }
+    
+    return sanitized;
+}
+
+export { validateAndSanitize };
