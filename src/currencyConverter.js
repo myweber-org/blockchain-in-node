6c26e7fe02860df80d2e9eab3678d2b2092f806f
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-07 20:54:50 +0000

    Add currency conversion utility with real-time exchange rates

--- src/currencyConverter.js
+++ src/currencyConverter.js
@@ -196,4 +196,44 @@ const CurrencyConverter = {
   getRate: fetchExchangeRate
 };
 
-export default CurrencyConverter;
+export default CurrencyConverter;const exchangeRates = {
+    USD: 1.0,
+    EUR: 0.85,
+    GBP: 0.73,
+    JPY: 110.0,
+    CAD: 1.25,
+    AUD: 1.35,
+    CNY: 6.45
+};
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+        throw new Error('Invalid currency code');
+    }
+    
+    const amountInUSD = amount / exchangeRates[fromCurrency];
+    const convertedAmount = amountInUSD * exchangeRates[toCurrency];
+    
+    return parseFloat(convertedAmount.toFixed(2));
+}
+
+function getAvailableCurrencies() {
+    return Object.keys(exchangeRates);
+}
+
+function updateExchangeRate(currency, newRate) {
+    if (!exchangeRates[currency]) {
+        throw new Error('Currency not found');
+    }
+    if (typeof newRate !== 'number' || newRate <= 0) {
+        throw new Error('Invalid exchange rate');
+    }
+    exchangeRates[currency] = newRate;
+}
+
+module.exports = {
+    convertCurrency,
+    getAvailableCurrencies,
+    updateExchangeRate,
+    exchangeRates
+};
