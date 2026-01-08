a8e18bc72074b30b56b86c46f3764ef060dffad7
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-08 19:23:07 +0000

    Add function to calculate area of a circle given radius

--- src/calculateCircleArea.js
+++ src/calculateCircleArea.js
@@ -2,20 +2,11 @@ function calculateCircleArea(radius) {
     if (typeof radius !== 'number' || radius <= 0) {
         throw new Error('Radius must be a positive number');
     }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
-}function calculateCircleArea(radius) {
-    if (typeof radius !== 'number' || radius <= 0) {
-        throw new Error('Radius must be a positive number');
-    }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
+    return Math.PI * radius * radius;
 }
 
-module.exports = calculateCircleArea;function calculateCircleArea(radius) {
-    if (typeof radius !== 'number' || radius <= 0) {
-        throw new Error('Radius must be a positive number');
-    }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
+function formatArea(area, precision = 2) {
+    return area.toFixed(precision);
 }
+
+module.exports = { calculateCircleArea, formatArea };
