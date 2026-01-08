2a473d2cc81616bdb99ff03c85751d0ac8835b07
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-08 01:11:53 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -16,57 +16,14 @@ function convertTemperature(value, unit) {
     }
 }
 
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-    if (unit === 'C') {
-        return {
-            celsius: value,
-            fahrenheit: celsiusToFahrenheit(value)
-        };
-    } else if (unit === 'F') {
-        return {
-            celsius: fahrenheitToCelsius(value),
-            fahrenheit: value
-        };
-    }
-    throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
+function formatTemperature(value, originalUnit) {
+    const convertedUnit = originalUnit.toLowerCase() === 'c' ? 'F' : 'C';
+    return `${value.toFixed(2)}°${convertedUnit}`;
 }
 
 module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function convertTemperature(value, unit) {
-    if (unit === 'C') {
-        return celsiusToFahrenheit(value);
-    } else if (unit === 'F') {
-        return fahrenheitToCelsius(value);
-    } else {
-        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
-    }
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    convertTemperature
+    convertTemperature,
+    formatTemperature
 };
