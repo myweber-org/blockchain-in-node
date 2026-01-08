b54e8ef6e38e465066b25c40167fcc0180b4c2f6
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-08 14:26:55 +0000

    Add input sanitization utility to prevent XSS attacks

--- src/dataSanitizer.js
+++ src/dataSanitizer.js
@@ -85,4 +85,33 @@ function validateAndSanitizeUserInput(userInput, maxLength = 1000) {
     return sanitizeInput(userInput);
 }
 
-export { sanitizeInput, validateAndSanitizeUserInput };
+export { sanitizeInput, validateAndSanitizeUserInput };function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        '/': '&#x2F;'
+    };
+    
+    const reg = /[&<>"'/]/ig;
+    return input.replace(reg, (match) => map[match]);
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function escapeHtml(text) {
+    const div = document.createElement('div');
+    div.textContent = text;
+    return div.innerHTML;
+}
+
+export { sanitizeInput, validateEmail, escapeHtml };
