ebbd15f9625b155ccfbb70803d6b9e74efd63fa1
InkspillVerse <inkspill.verse@outlook.com>
2026-01-08 14:50:49 +0000

    Add input validation function for username and password

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -161,4 +161,29 @@ function validatePassword(password) {
            hasSpecialChar;
 }
 
-export { sanitizeInput, validateEmail, validatePassword };
+export { sanitizeInput, validateEmail, validatePassword };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: 'Invalid username and password format' };
+    } else if (!usernameValid) {
+        return { valid: false, message: 'Invalid username format' };
+    } else if (!passwordValid) {
+        return { valid: false, message: 'Invalid password format' };
+    }
+    
+    return { valid: true, message: 'Input validation successful' };
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
