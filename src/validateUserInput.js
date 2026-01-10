a8f5b34adaea0ec806999172e85abd6d15a42482
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-10 02:41:10 +0000

    Add input validation for username and password fields

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -101,4 +101,42 @@ function validateUserInput(username, email) {
     };
 }
 
-module.exports = { validateUserInput, validateUsername, validateEmail };
+module.exports = { validateUserInput, validateUsername, validateEmail };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: "Invalid username and password" };
+    }
+    
+    if (!usernameValid) {
+        return { valid: false, message: "Invalid username format" };
+    }
+    
+    if (!passwordValid) {
+        return { valid: false, message: "Password does not meet requirements" };
+    }
+    
+    return { valid: true, message: "Input validation successful" };
+}
+
+export { validateUserInput, validateUsername, validatePassword };
