24bb953574c5470733aaea36832b8c0d3cef51f1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-11 01:32:04 +0000

    Add function to calculate area of a circle given its radius

--- src/calculateCircleArea.js
+++ src/calculateCircleArea.js
@@ -1,7 +1,12 @@
 function calculateCircleArea(radius) {
-    if (typeof radius !== 'number' || radius <= 0) {
-        throw new Error('Radius must be a positive number');
+    if (typeof radius !== 'number' || radius < 0) {
+        throw new Error('Radius must be a non-negative number');
     }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
+    return Math.PI * radius * radius;
 }
+
+function formatArea(area, precision = 2) {
+    return area.toFixed(precision);
+}
+
+module.exports = { calculateCircleArea, formatArea };
