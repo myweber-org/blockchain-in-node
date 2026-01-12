a89ed22a8fb575d87e75fd5b37f2e2bd4159e868
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-12 15:58:23 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and Fahrenheit to Celsius functions

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -64,4 +64,45 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
-};
+};function celsiusToFahrenheit(celsius) {
+    if (typeof celsius !== 'number' || isNaN(celsius)) {
+        throw new Error('Input must be a valid number');
+    }
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    if (typeof fahrenheit !== 'number' || isNaN(fahrenheit)) {
+        throw new Error('Input must be a valid number');
+    }
+    return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+    const validUnits = ['C', 'F'];
+    unit = unit.toUpperCase();
+    
+    if (!validUnits.includes(unit)) {
+        throw new Error('Unit must be "C" for Celsius or "F" for Fahrenheit');
+    }
+    
+    if (typeof value !== 'number' || isNaN(value)) {
+        throw new Error('Value must be a valid number');
+    }
+    
+    if (unit === 'C') {
+        return {
+            celsius: value,
+            fahrenheit: celsiusToFahrenheit(value),
+            originalUnit: 'C'
+        };
+    } else {
+        return {
+            celsius: fahrenheitToCelsius(value),
+            fahrenheit: value,
+            originalUnit: 'F'
+        };
+    }
+}
+
+export { celsiusToFahrenheit, fahrenheitToCelsius, convertTemperature };
