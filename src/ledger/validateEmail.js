552f9fbe47c44b138a206d8c00d168b256e8975f
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-12 15:19:11 +0000

    Add email validation utility function with regex pattern

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -2,15 +2,3 @@ function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
-
-module.exports = validateEmail;function validateEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
-
-module.exports = validateEmail;
