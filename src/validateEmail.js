53932d1caad49f21a432fabf72112516917d1c36
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-13 23:47:30 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -8,4 +8,7 @@ module.exports = validateEmail;function validateEmail(email) {
   return emailRegex.test(email);
 }
 
-module.exports = validateEmail;
+module.exports = validateEmail;function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
