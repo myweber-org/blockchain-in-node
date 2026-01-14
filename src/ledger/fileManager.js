44917d40c597f579414ffd61952a71219a089cb3
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-14 20:47:25 +0000

    Add file management utility with read, write, and delete operations

--- src/ledger/fileManager.js
+++ src/ledger/fileManager.js
@@ -53,3 +53,52 @@ class FileManager {
 }
 
 module.exports = FileManager;
+const fs = require('fs');
+const path = require('path');
+
+class FileManager {
+  constructor(basePath) {
+    this.basePath = basePath;
+  }
+
+  readFile(filename) {
+    const filePath = path.join(this.basePath, filename);
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  }
+
+  writeFile(filename, content) {
+    const filePath = path.join(this.basePath, filename);
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filePath, content, 'utf8', (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+
+  deleteFile(filename) {
+    const filePath = path.join(this.basePath, filename);
+    return new Promise((resolve, reject) => {
+      fs.unlink(filePath, (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+
+  listFiles() {
+    return new Promise((resolve, reject) => {
+      fs.readdir(this.basePath, (err, files) => {
+        if (err) reject(err);
+        else resolve(files);
+      });
+    });
+  }
+}
+
+module.exports = FileManager;
