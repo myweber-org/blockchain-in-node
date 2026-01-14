ddd2bfa27045b2cb3cc3a2d49dd5eeb1945ad0e0
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-14 15:16:54 +0000

    Add email validation utility with regex pattern

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -1,14 +1,6 @@
 function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-}function validateEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
 }
 
 module.exports = validateEmail;
