137652ffab3a703d344340b1752c51798c3f5ff6
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-15 00:03:05 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and Fahrenheit to Celsius functions

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -7,13 +7,12 @@ function fahrenheitToCelsius(fahrenheit) {
 }
 
 function convertTemperature(value, unit) {
-    if (unit.toLowerCase() === 'c') {
+    if (unit === 'C') {
         return celsiusToFahrenheit(value);
-    } else if (unit.toLowerCase() === 'f') {
+    } else if (unit === 'F') {
         return fahrenheitToCelsius(value);
-    } else {
-        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
     }
+    return null;
 }
 
 module.exports = {
