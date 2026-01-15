9353ec808af9b6c2dcf45c71c3ef5c3fd55fbaae
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-15 22:33:15 +0000

    Add function to compute the nth Fibonacci number using memoization

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -5,4 +5,11 @@ function fibonacci(n, memo = {}) {
     return memo[n];
 }
 
+module.exports = fibonacci;function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
+}
+
 module.exports = fibonacci;
