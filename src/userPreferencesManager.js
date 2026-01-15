d928681a4a605db7641a62f1e7bfab40c3adde47
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-15 19:56:33 +0000

    Add user preferences management with localStorage support

--- src/userPreferencesManager.js
+++ src/userPreferencesManager.js
@@ -120,4 +120,54 @@ class UserPreferencesManager {
     };
 })();
 
-export default UserPreferencesManager;
+export default UserPreferencesManager;const UserPreferencesManager = (function() {
+    const STORAGE_KEY = 'user_preferences';
+    
+    function getPreferences() {
+        const stored = localStorage.getItem(STORAGE_KEY);
+        return stored ? JSON.parse(stored) : {};
+    }
+    
+    function setPreference(key, value) {
+        const preferences = getPreferences();
+        preferences[key] = value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(preferences));
+        return true;
+    }
+    
+    function getPreference(key, defaultValue = null) {
+        const preferences = getPreferences();
+        return preferences.hasOwnProperty(key) ? preferences[key] : defaultValue;
+    }
+    
+    function removePreference(key) {
+        const preferences = getPreferences();
+        if (preferences.hasOwnProperty(key)) {
+            delete preferences[key];
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(preferences));
+            return true;
+        }
+        return false;
+    }
+    
+    function clearAllPreferences() {
+        localStorage.removeItem(STORAGE_KEY);
+        return true;
+    }
+    
+    function getAllPreferences() {
+        return getPreferences();
+    }
+    
+    return {
+        get: getPreference,
+        set: setPreference,
+        remove: removePreference,
+        clear: clearAllPreferences,
+        getAll: getAllPreferences
+    };
+})();
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = UserPreferencesManager;
+}
