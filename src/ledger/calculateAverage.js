7690b71f0b3cb9e901a4839f8f1c429173f9cd64
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-15 23:24:48 +0000

    Add function to calculate average of an array of numbers

--- src/ledger/calculateAverage.js
+++ src/ledger/calculateAverage.js
@@ -2,6 +2,7 @@ function calculateAverage(numbers) {
     if (!Array.isArray(numbers) || numbers.length === 0) {
         return 0;
     }
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
     return sum / numbers.length;
 }
