ef3521ef7c81c14dabb8e0e23f37976edb7f3f10
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-15 17:10:22 +0000

    Add function to compute nth Fibonacci number using memoization

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -1,9 +1,8 @@
-function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
-    }
-    return sequence;
+function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
+
+module.exports = fibonacci;
