a9c6ec0408fb0a4c2703ee8836312a43e1550848
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-15 15:21:10 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa functions

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -64,4 +64,31 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+    if (unit.toLowerCase() === 'c') {
+        return {
+            celsius: value,
+            fahrenheit: celsiusToFahrenheit(value)
+        };
+    } else if (unit.toLowerCase() === 'f') {
+        return {
+            celsius: fahrenheitToCelsius(value),
+            fahrenheit: value
+        };
+    }
+    throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius,
+    convertTemperature
 };
