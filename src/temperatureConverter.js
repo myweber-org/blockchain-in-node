54e325fb3151bd18f669e9912d233e8dd8ca5042
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-15 18:45:43 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa functions

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -2,64 +2,6 @@ function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
-function celsiusToKelvin(celsius) {
-    return celsius + 273.15;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function fahrenheitToKelvin(fahrenheit) {
-    return (fahrenheit - 32) * 5/9 + 273.15;
-}
-
-function kelvinToCelsius(kelvin) {
-    return kelvin - 273.15;
-}
-
-function kelvinToFahrenheit(kelvin) {
-    return (kelvin - 273.15) * 9/5 + 32;
-}
-
-function convertTemperature(value, fromUnit, toUnit) {
-    const conversions = {
-        'C': {
-            'F': celsiusToFahrenheit,
-            'K': celsiusToKelvin,
-            'C': (x) => x
-        },
-        'F': {
-            'C': fahrenheitToCelsius,
-            'K': fahrenheitToKelvin,
-            'F': (x) => x
-        },
-        'K': {
-            'C': kelvinToCelsius,
-            'F': kelvinToFahrenheit,
-            'K': (x) => x
-        }
-    };
-
-    if (conversions[fromUnit] && conversions[fromUnit][toUnit]) {
-        return conversions[fromUnit][toUnit](value);
-    }
-    
-    throw new Error('Invalid temperature units provided');
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    celsiusToKelvin,
-    fahrenheitToCelsius,
-    fahrenheitToKelvin,
-    kelvinToCelsius,
-    kelvinToFahrenheit,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
 function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5/9;
 }
