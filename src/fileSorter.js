560f3246218ba35f19f573d670980a7f61ea6dff
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-15 00:59:19 +0000

    Add utility function to sort files by extension and size

--- src/fileSorter.js
+++ src/fileSorter.js
@@ -31,3 +31,58 @@ function getTotalSize(files) {
 }
 
 export { sortFiles, groupFilesByExtension, getTotalSize };
+function sortFilesByExtensionAndSize(files) {
+    return files.sort((a, b) => {
+        const extA = a.name.split('.').pop().toLowerCase();
+        const extB = b.name.split('.').pop().toLowerCase();
+        
+        if (extA !== extB) {
+            return extA.localeCompare(extB);
+        }
+        
+        return a.size - b.size;
+    });
+}
+
+function validateFileList(files) {
+    if (!Array.isArray(files)) {
+        throw new TypeError('Input must be an array of file objects');
+    }
+    
+    return files.every(file => 
+        file && 
+        typeof file === 'object' && 
+        'name' in file && 
+        'size' in file
+    );
+}
+
+function getFileStatistics(files) {
+    if (!validateFileList(files)) {
+        return null;
+    }
+    
+    const sortedFiles = sortFilesByExtensionAndSize(files);
+    const stats = {
+        totalFiles: sortedFiles.length,
+        totalSize: sortedFiles.reduce((sum, file) => sum + file.size, 0),
+        extensions: {}
+    };
+    
+    sortedFiles.forEach(file => {
+        const ext = file.name.split('.').pop().toLowerCase();
+        if (!stats.extensions[ext]) {
+            stats.extensions[ext] = { count: 0, totalSize: 0 };
+        }
+        stats.extensions[ext].count++;
+        stats.extensions[ext].totalSize += file.size;
+    });
+    
+    return stats;
+}
+
+module.exports = {
+    sortFilesByExtensionAndSize,
+    validateFileList,
+    getFileStatistics
+};
