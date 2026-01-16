f2f8fdaae7665e09314da743c13e088f22f6cb77
InkspillVerse <inkspill.verse@outlook.com>
2026-01-16 22:30:15 +0000

    Add utility function to sort files by extension and size

--- src/fileSorter.js
+++ src/fileSorter.js
@@ -85,4 +85,15 @@ module.exports = {
     sortFilesByExtensionAndSize,
     validateFileList,
     getFileStatistics
-};
+};function sortFiles(files) {
+    return files.sort((a, b) => {
+        const extA = a.name.split('.').pop().toLowerCase();
+        const extB = b.name.split('.').pop().toLowerCase();
+        if (extA !== extB) {
+            return extA.localeCompare(extB);
+        }
+        return a.size - b.size;
+    });
+}
+
+export { sortFiles };
