dba053331cea34f44483451e92fe2fcaa312109b
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-17 23:31:38 +0000

    Add input validation utility for username and email fields

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -3,28 +3,26 @@ function validateUsername(username) {
     return usernameRegex.test(username);
 }
 
-function validatePassword(password) {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return passwordRegex.test(password);
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
 
-function validateUserInput(username, password) {
-    const usernameValid = validateUsername(username);
-    const passwordValid = validatePassword(password);
+function validateUserInput(username, email) {
+    const errors = [];
     
-    if (!usernameValid && !passwordValid) {
-        return 'Username and password are invalid';
-    } else if (!usernameValid) {
-        return 'Username is invalid';
-    } else if (!passwordValid) {
-        return 'Password is invalid';
-    } else {
-        return 'User input is valid';
+    if (!validateUsername(username)) {
+        errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores.');
     }
+    
+    if (!validateEmail(email)) {
+        errors.push('Please enter a valid email address.');
+    }
+    
+    return {
+        isValid: errors.length === 0,
+        errors: errors
+    };
 }
 
-module.exports = {
-    validateUsername,
-    validatePassword,
-    validateUserInput
-};
+export { validateUserInput, validateUsername, validateEmail };
