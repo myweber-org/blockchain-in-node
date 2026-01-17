84cbb91566cd2c34bece6bcb549820680b098482
InkspillVerse <inkspill.verse@outlook.com>
2026-01-17 15:11:24 +0000

    Add input validation function to sanitize user-provided strings

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -97,4 +97,26 @@ export { validateUserInput, validateUsername, validatePassword };function valida
     };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/[<>]/g, '')
+        .replace(/javascript:/gi, '')
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 8 &&
+           /[A-Z]/.test(password) &&
+           /[a-z]/.test(password) &&
+           /\d/.test(password);
+}
+
+module.exports = { sanitizeInput, validateEmail, validatePassword };
