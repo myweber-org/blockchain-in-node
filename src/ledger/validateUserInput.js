57d2dd6c345271f9d3d7b3686771ca50a686b0f2
InkspillVerse <inkspill.verse@outlook.com>
2026-01-18 01:08:45 +0000

    Add input validation for username and password fields

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -56,4 +56,38 @@ function validateUserInput(username, password) {
     };
 }
 
-export { validateUserInput, validateUsername, validatePassword };
+export { validateUserInput, validateUsername, validatePassword };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return 'Username and password are invalid';
+    } else if (!usernameValid) {
+        return 'Username is invalid';
+    } else if (!passwordValid) {
+        return 'Password is invalid';
+    }
+    
+    return 'Validation passed';
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
