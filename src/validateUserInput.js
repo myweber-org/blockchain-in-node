c212193114e11740a2c834dda26b9a2950ffb160
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-18 21:15:53 +0000

    Add input validation function for username and email fields

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -1,31 +1,15 @@
-function validateUsername(username) {
+function validateUserInput(username, email) {
     const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-    return usernameRegex.test(username);
-}
-
-function validatePassword(password) {
-    const minLength = 8;
-    const hasUpperCase = /[A-Z]/.test(password);
-    const hasLowerCase = /[a-z]/.test(password);
-    const hasNumbers = /\d/.test(password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
-    return password.length >= minLength && 
-           hasUpperCase && 
-           hasLowerCase && 
-           hasNumbers && 
-           hasSpecialChar;
-}
-
-function validateUserInput(username, password) {
     const errors = [];
     
-    if (!validateUsername(username)) {
-        errors.push("Username must be 3-20 characters and contain only letters, numbers, and underscores.");
+    if (!usernameRegex.test(username)) {
+        errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores.');
     }
     
-    if (!validatePassword(password)) {
-        errors.push("Password must be at least 8 characters long and contain uppercase, lowercase, numbers, and special characters.");
+    if (!emailRegex.test(email)) {
+        errors.push('Please enter a valid email address.');
     }
     
     return {
@@ -33,39 +17,3 @@ function validateUserInput(username, password) {
         errors: errors
     };
 }
-
-module.exports = { validateUserInput, validateUsername, validatePassword };function validateUsername(username) {
-    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-    return usernameRegex.test(username);
-}
-
-function validatePassword(password) {
-    if (password.length < 8) {
-        return false;
-    }
-    const hasUpperCase = /[A-Z]/.test(password);
-    const hasLowerCase = /[a-z]/.test(password);
-    const hasNumber = /[0-9]/.test(password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-    return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
-}
-
-function validateUserInput(username, password) {
-    const usernameValid = validateUsername(username);
-    const passwordValid = validatePassword(password);
-    
-    if (!usernameValid && !passwordValid) {
-        return 'Username and password are invalid';
-    } else if (!usernameValid) {
-        return 'Username is invalid';
-    } else if (!passwordValid) {
-        return 'Password is invalid';
-    }
-    return 'User input is valid';
-}
-
-module.exports = {
-    validateUsername,
-    validatePassword,
-    validateUserInput
-};
