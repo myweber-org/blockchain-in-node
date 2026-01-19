e8130aca0ad7c75806f80f6b55ce34432394e79d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-19 14:52:44 +0000

    Implement Fibonacci sequence calculation with memoization

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -7,4 +7,19 @@ function calculateFibonacci(n) {
         sequence.push(sequence[i - 1] + sequence[i - 2]);
     }
     return sequence;
+}function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 1) return n;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
+
+function generateFibonacciSequence(length) {
+    const sequence = [];
+    for (let i = 0; i < length; i++) {
+        sequence.push(fibonacci(i));
+    }
+    return sequence;
+}
+
+module.exports = { fibonacci, generateFibonacciSequence };
