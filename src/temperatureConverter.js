76d99ba6668006a5b49faf8707797ead5cb48ccf
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-20 14:39:37 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and Kelvin functions

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -20,4 +20,67 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function celsiusToKelvin(celsius) {
+    return celsius + 273.15;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function fahrenheitToKelvin(fahrenheit) {
+    return celsiusToKelvin(fahrenheitToCelsius(fahrenheit));
+}
+
+function kelvinToCelsius(kelvin) {
+    return kelvin - 273.15;
+}
+
+function kelvinToFahrenheit(kelvin) {
+    return celsiusToFahrenheit(kelvinToCelsius(kelvin));
+}
+
+function convertTemperature(value, fromUnit, toUnit) {
+    const units = ['C', 'F', 'K'];
+    fromUnit = fromUnit.toUpperCase();
+    toUnit = toUnit.toUpperCase();
+    
+    if (!units.includes(fromUnit) || !units.includes(toUnit)) {
+        throw new Error('Invalid temperature unit');
+    }
+    
+    if (fromUnit === toUnit) {
+        return value;
+    }
+    
+    const conversionMap = {
+        'C': {
+            'F': celsiusToFahrenheit,
+            'K': celsiusToKelvin
+        },
+        'F': {
+            'C': fahrenheitToCelsius,
+            'K': fahrenheitToKelvin
+        },
+        'K': {
+            'C': kelvinToCelsius,
+            'F': kelvinToFahrenheit
+        }
+    };
+    
+    return conversionMap[fromUnit][toUnit](value);
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    celsiusToKelvin,
+    fahrenheitToCelsius,
+    fahrenheitToKelvin,
+    kelvinToCelsius,
+    kelvinToFahrenheit,
+    convertTemperature
 };
