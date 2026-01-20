042966e0a192fe89948ef2e6f9b72221ab48a198
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-20 19:05:46 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa functions

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -16,71 +16,14 @@ function convertTemperature(value, unit) {
     }
 }
 
-module.exports = {
-    celsiusToFahrenheit,
-    fahrenheitToCelsius,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function celsiusToKelvin(celsius) {
-    return celsius + 273.15;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function fahrenheitToKelvin(fahrenheit) {
-    return celsiusToKelvin(fahrenheitToCelsius(fahrenheit));
-}
-
-function kelvinToCelsius(kelvin) {
-    return kelvin - 273.15;
-}
-
-function kelvinToFahrenheit(kelvin) {
-    return celsiusToFahrenheit(kelvinToCelsius(kelvin));
-}
-
-function convertTemperature(value, fromUnit, toUnit) {
-    const units = ['C', 'F', 'K'];
-    fromUnit = fromUnit.toUpperCase();
-    toUnit = toUnit.toUpperCase();
-    
-    if (!units.includes(fromUnit) || !units.includes(toUnit)) {
-        throw new Error('Invalid temperature unit');
-    }
-    
-    if (fromUnit === toUnit) {
-        return value;
-    }
-    
-    const conversionMap = {
-        'C': {
-            'F': celsiusToFahrenheit,
-            'K': celsiusToKelvin
-        },
-        'F': {
-            'C': fahrenheitToCelsius,
-            'K': fahrenheitToKelvin
-        },
-        'K': {
-            'C': kelvinToCelsius,
-            'F': kelvinToFahrenheit
-        }
-    };
-    
-    return conversionMap[fromUnit][toUnit](value);
+function formatTemperature(value, fromUnit, toUnit) {
+    const converted = convertTemperature(value, fromUnit);
+    return `${value}°${fromUnit.toUpperCase()} = ${converted.toFixed(2)}°${toUnit.toUpperCase()}`;
 }
 
 module.exports = {
     celsiusToFahrenheit,
-    celsiusToKelvin,
     fahrenheitToCelsius,
-    fahrenheitToKelvin,
-    kelvinToCelsius,
-    kelvinToFahrenheit,
-    convertTemperature
+    convertTemperature,
+    formatTemperature
 };
