fa93cded7b7076ab810ff4d9beecd83acd3e24ec
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-20 20:09:27 +0000

    Add fileManager module with utility functions for handling file operations

--- src/ledger/fileManager.js
+++ src/ledger/fileManager.js
@@ -144,4 +144,58 @@ class FileManager {
     }
 }
 
+module.exports = FileManager;const fs = require('fs');
+const path = require('path');
+
+class FileManager {
+  constructor(basePath) {
+    this.basePath = basePath;
+  }
+
+  readFile(filePath) {
+    const fullPath = path.join(this.basePath, filePath);
+    return new Promise((resolve, reject) => {
+      fs.readFile(fullPath, 'utf8', (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  }
+
+  writeFile(filePath, content) {
+    const fullPath = path.join(this.basePath, filePath);
+    return new Promise((resolve, reject) => {
+      fs.writeFile(fullPath, content, 'utf8', (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+
+  listFiles(directoryPath) {
+    const fullPath = path.join(this.basePath, directoryPath);
+    return new Promise((resolve, reject) => {
+      fs.readdir(fullPath, (err, files) => {
+        if (err) reject(err);
+        else resolve(files);
+      });
+    });
+  }
+
+  getFileStats(filePath) {
+    const fullPath = path.join(this.basePath, filePath);
+    return new Promise((resolve, reject) => {
+      fs.stat(fullPath, (err, stats) => {
+        if (err) reject(err);
+        else resolve(stats);
+      });
+    });
+  }
+
+  static validateFileName(fileName) {
+    const invalidChars = /[<>:"/\\|?*]/;
+    return !invalidChars.test(fileName);
+  }
+}
+
 module.exports = FileManager;
