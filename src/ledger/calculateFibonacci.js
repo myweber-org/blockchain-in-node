f06a5b3464ffea7d603ade0404f6c8dcbb9a158c
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-20 18:51:03 +0000

    Add function to compute nth Fibonacci number using memoization

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -5,25 +5,4 @@ function fibonacci(n, memo = {}) {
     return memo[n];
 }
 
-module.exports = fibonacci;function fibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 2) return 1;
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
-    return memo[n];
-}
-
 module.exports = fibonacci;
-function fibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 1) return n;
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
-    return memo[n];
-}
-
-function printFibonacciSequence(limit) {
-    for (let i = 0; i <= limit; i++) {
-        console.log(`Fibonacci(${i}) = ${fibonacci(i)}`);
-    }
-}
-
-module.exports = { fibonacci, printFibonacciSequence };
