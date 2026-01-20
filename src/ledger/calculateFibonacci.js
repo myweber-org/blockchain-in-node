6d5f9f65a54c16a270b069c7824f2b406138ef95
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-20 00:04:46 +0000

    Add function to calculate the nth Fibonacci number using memoization for efficiency

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -13,3 +13,17 @@ module.exports = fibonacci;function fibonacci(n, memo = {}) {
 }
 
 module.exports = fibonacci;
+function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 1) return n;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
+}
+
+function printFibonacciSequence(limit) {
+    for (let i = 0; i <= limit; i++) {
+        console.log(`Fibonacci(${i}) = ${fibonacci(i)}`);
+    }
+}
+
+module.exports = { fibonacci, printFibonacciSequence };
