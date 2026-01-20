f35b92b7af8b6eec6e781def8e95c048603900ef
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-20 19:28:20 +0000

    Add input validation for username and password fields

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -71,4 +71,29 @@ module.exports = validateUserInput;function validateUserInput(username, password
         isValid: true,
         message: "Input validation passed."
     };
+}function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
 }
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid) {
+        throw new Error('Invalid username format');
+    }
+    
+    if (!passwordValid) {
+        throw new Error('Password must be at least 8 characters with uppercase, lowercase, number and special character');
+    }
+    
+    return { usernameValid, passwordValid };
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
