9fc1b7cc4e522dc82e18778c7fb7f1f105ba6033
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-20 00:47:52 +0000

    Add user preferences manager with localStorage support

--- src/userPreferencesManager.js
+++ src/userPreferencesManager.js
@@ -64,4 +64,49 @@ const UserPreferencesManager = (function() {
         reset: resetPreferences,
         subscribe: subscribe
     };
+})();const userPreferencesManager = (function() {
+    const PREFERENCES_KEY = 'app_preferences';
+
+    function getPreferences() {
+        const stored = localStorage.getItem(PREFERENCES_KEY);
+        return stored ? JSON.parse(stored) : {};
+    }
+
+    function setPreference(key, value) {
+        const preferences = getPreferences();
+        preferences[key] = value;
+        localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+        return preferences;
+    }
+
+    function removePreference(key) {
+        const preferences = getPreferences();
+        delete preferences[key];
+        localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+        return preferences;
+    }
+
+    function clearAllPreferences() {
+        localStorage.removeItem(PREFERENCES_KEY);
+        return {};
+    }
+
+    function hasPreference(key) {
+        const preferences = getPreferences();
+        return key in preferences;
+    }
+
+    function getAllKeys() {
+        const preferences = getPreferences();
+        return Object.keys(preferences);
+    }
+
+    return {
+        get: getPreferences,
+        set: setPreference,
+        remove: removePreference,
+        clear: clearAllPreferences,
+        has: hasPreference,
+        keys: getAllKeys
+    };
 })();
