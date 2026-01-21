c70586eab7d005f190e5ea2d8da7efc8c74ca639
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-21 00:04:28 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -5,4 +5,27 @@ function fibonacci(n, memo = {}) {
     return memo[n];
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci;function calculateFibonacci(limit) {
+    const sequence = [0, 1];
+    
+    if (limit <= 0) return [];
+    if (limit === 1) return [0];
+    
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
+        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+        sequence.push(nextValue);
+    }
+    
+    return sequence;
+}
+
+function isFibonacciNumber(num) {
+    if (num < 0) return false;
+    
+    const check1 = Math.sqrt(5 * num * num + 4);
+    const check2 = Math.sqrt(5 * num * num - 4);
+    
+    return Number.isInteger(check1) || Number.isInteger(check2);
+}
+
+module.exports = { calculateFibonacci, isFibonacciNumber };
