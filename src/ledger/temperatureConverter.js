05451cbd888a126948c0125bcb74805fa88f188c
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-21 19:55:27 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa functions

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -2,66 +2,6 @@ function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
-function celsiusToKelvin(celsius) {
-    return celsius + 273.15;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-
-function fahrenheitToKelvin(fahrenheit) {
-    return (fahrenheit - 32) * 5/9 + 273.15;
-}
-
-function kelvinToCelsius(kelvin) {
-    return kelvin - 273.15;
-}
-
-function kelvinToFahrenheit(kelvin) {
-    return (kelvin - 273.15) * 9/5 + 32;
-}
-
-function convertTemperature(value, fromUnit, toUnit) {
-    const units = ['C', 'F', 'K'];
-    if (!units.includes(fromUnit) || !units.includes(toUnit)) {
-        throw new Error('Invalid temperature unit');
-    }
-
-    if (fromUnit === toUnit) {
-        return value;
-    }
-
-    const conversionMap = {
-        'C': {
-            'F': celsiusToFahrenheit,
-            'K': celsiusToKelvin
-        },
-        'F': {
-            'C': fahrenheitToCelsius,
-            'K': fahrenheitToKelvin
-        },
-        'K': {
-            'C': kelvinToCelsius,
-            'F': kelvinToFahrenheit
-        }
-    };
-
-    return conversionMap[fromUnit][toUnit](value);
-}
-
-module.exports = {
-    celsiusToFahrenheit,
-    celsiusToKelvin,
-    fahrenheitToCelsius,
-    fahrenheitToKelvin,
-    kelvinToCelsius,
-    kelvinToFahrenheit,
-    convertTemperature
-};function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
 function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5/9;
 }
