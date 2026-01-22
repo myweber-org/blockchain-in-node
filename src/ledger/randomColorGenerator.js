8a0825bdb1580e957df9710520e134b772936631
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-22 00:17:17 +0000

    Add random color generator utility function

--- src/ledger/randomColorGenerator.js
+++ src/ledger/randomColorGenerator.js
@@ -1,5 +1,4 @@
-
-function getRandomColor() {
+function generateRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
   for (let i = 0; i < 6; i++) {
@@ -8,35 +7,18 @@ function getRandomColor() {
   return color;
 }
 
-function generateRandomColors(count) {
-  const colors = [];
-  for (let i = 0; i < count; i++) {
-    colors.push(getRandomColor());
-  }
-  return colors;
-}
-
-export { getRandomColor, generateRandomColors };function generateRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function generateRandomRGB() {
+function generateRandomRgbColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-function generateRandomHSL() {
+function generateRandomHslColor() {
   const h = Math.floor(Math.random() * 360);
   const s = Math.floor(Math.random() * 101);
   const l = Math.floor(Math.random() * 101);
   return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
-export { generateRandomColor, generateRandomRGB, generateRandomHSL };
+export { generateRandomColor, generateRandomRgbColor, generateRandomHslColor };
