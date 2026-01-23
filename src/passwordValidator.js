c71e0dfabe24a77a4c73e9998b3557861f534c07
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-23 20:06:14 +0000

    Add password strength validation utility with regex patterns

--- src/passwordValidator.js
+++ src/passwordValidator.js
@@ -44,4 +44,32 @@ function calculatePasswordStrength(password) {
     return 'Weak';
 }
 
-module.exports = { validatePassword, calculatePasswordStrength };
+module.exports = { validatePassword, calculatePasswordStrength };function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    if (password.length < minLength) {
+        return { valid: false, message: 'Password must be at least 8 characters long' };
+    }
+    
+    if (!hasUpperCase) {
+        return { valid: false, message: 'Password must contain at least one uppercase letter' };
+    }
+    
+    if (!hasLowerCase) {
+        return { valid: false, message: 'Password must contain at least one lowercase letter' };
+    }
+    
+    if (!hasNumbers) {
+        return { valid: false, message: 'Password must contain at least one number' };
+    }
+    
+    if (!hasSpecialChar) {
+        return { valid: false, message: 'Password must contain at least one special character' };
+    }
+    
+    return { valid: true, message: 'Password meets all requirements' };
+}
