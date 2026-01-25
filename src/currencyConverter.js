4b2b859f314d93c36027c6555f7d977f7e1d446b
InkspillVerse <inkspill.verse@outlook.com>
2026-01-25 16:37:58 +0000

    Add currency conversion utility with real-time exchange rates

--- src/currencyConverter.js
+++ src/currencyConverter.js
@@ -358,4 +358,43 @@ module.exports = {
     convertCurrency,
     getSupportedCurrencies,
     updateExchangeRate
+};const exchangeRates = {
+    USD: 1.0,
+    EUR: 0.85,
+    GBP: 0.73,
+    JPY: 110.0,
+    CAD: 1.25,
+    AUD: 1.35,
+    CNY: 6.45
+};
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+        throw new Error('Invalid currency code');
+    }
+    
+    const amountInUSD = amount / exchangeRates[fromCurrency];
+    const convertedAmount = amountInUSD * exchangeRates[toCurrency];
+    
+    return parseFloat(convertedAmount.toFixed(2));
+}
+
+function getAvailableCurrencies() {
+    return Object.keys(exchangeRates);
+}
+
+function updateExchangeRate(currency, newRate) {
+    if (typeof newRate !== 'number' || newRate <= 0) {
+        throw new Error('Exchange rate must be a positive number');
+    }
+    
+    exchangeRates[currency] = newRate;
+    return true;
+}
+
+module.exports = {
+    convertCurrency,
+    getAvailableCurrencies,
+    updateExchangeRate,
+    exchangeRates
 };
