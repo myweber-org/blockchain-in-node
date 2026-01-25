5ca2c0f41005a3b9418fc0fc714135f0970c8949
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-25 23:29:48 +0000

    Add input validation utility for username and email fields

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -3,49 +3,20 @@ function validateUsername(username) {
     return usernameRegex.test(username);
 }
 
-function validatePassword(password) {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return passwordRegex.test(password);
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
 
-function validateUserInput(username, password) {
-    const usernameValid = validateUsername(username);
-    const passwordValid = validatePassword(password);
-    
-    if (!usernameValid && !passwordValid) {
-        return 'Username and password are invalid';
-    } else if (!usernameValid) {
-        return 'Username is invalid';
-    } else if (!passwordValid) {
-        return 'Password is invalid';
-    } else {
-        return 'User input is valid';
-    }
-}
-
-module.exports = {
-    validateUsername,
-    validatePassword,
-    validateUserInput
-};function validateUsername(username) {
-    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-    return usernameRegex.test(username);
-}
-
-function validatePassword(password) {
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/;
-    return passwordRegex.test(password);
-}
-
-function validateUserInput(username, password) {
+function validateUserInput(username, email) {
     const errors = [];
     
     if (!validateUsername(username)) {
-        errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores.');
+        errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores');
     }
     
-    if (!validatePassword(password)) {
-        errors.push('Password must be at least 8 characters and contain at least one letter and one number.');
+    if (!validateEmail(email)) {
+        errors.push('Please enter a valid email address');
     }
     
     return {
@@ -54,4 +25,4 @@ function validateUserInput(username, password) {
     };
 }
 
-module.exports = validateUserInput;
+export { validateUsername, validateEmail, validateUserInput };
