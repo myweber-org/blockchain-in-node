e5dd03c53447f073b58172fbe07f7c7ac850f1da
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-25 14:30:20 +0000

    Add user preferences management with local storage support

--- src/userPreferencesManager.js
+++ src/userPreferencesManager.js
@@ -1,51 +1,39 @@
-const UserPreferencesManager = (function() {
-    const STORAGE_KEY = 'user_preferences';
-    
-    function getPreferences() {
-        const stored = localStorage.getItem(STORAGE_KEY);
-        return stored ? JSON.parse(stored) : {};
-    }
-    
-    function setPreference(key, value) {
-        const preferences = getPreferences();
-        preferences[key] = value;
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(preferences));
-        return true;
-    }
-    
-    function getPreference(key, defaultValue = null) {
-        const preferences = getPreferences();
-        return preferences.hasOwnProperty(key) ? preferences[key] : defaultValue;
-    }
-    
-    function removePreference(key) {
-        const preferences = getPreferences();
-        if (preferences.hasOwnProperty(key)) {
-            delete preferences[key];
-            localStorage.setItem(STORAGE_KEY, JSON.stringify(preferences));
-            return true;
-        }
-        return false;
-    }
-    
-    function clearAllPreferences() {
-        localStorage.removeItem(STORAGE_KEY);
-        return true;
-    }
-    
-    function getAllPreferences() {
-        return getPreferences();
-    }
-    
-    return {
-        set: setPreference,
-        get: getPreference,
-        remove: removePreference,
-        clear: clearAllPreferences,
-        getAll: getAllPreferences
-    };
-})();
+const UserPreferences = {
+  _prefs: {},
 
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = UserPreferencesManager;
-}
+  init() {
+    const stored = localStorage.getItem('userPreferences');
+    if (stored) {
+      this._prefs = JSON.parse(stored);
+    }
+  },
+
+  set(key, value) {
+    this._prefs[key] = value;
+    this._save();
+  },
+
+  get(key, defaultValue = null) {
+    return this._prefs[key] !== undefined ? this._prefs[key] : defaultValue;
+  },
+
+  remove(key) {
+    delete this._prefs[key];
+    this._save();
+  },
+
+  clear() {
+    this._prefs = {};
+    localStorage.removeItem('userPreferences');
+  },
+
+  getAll() {
+    return { ...this._prefs };
+  },
+
+  _save() {
+    localStorage.setItem('userPreferences', JSON.stringify(this._prefs));
+  }
+};
+
+UserPreferences.init();
