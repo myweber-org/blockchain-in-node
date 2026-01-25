9ec59a9de75b7b1f2bf1a6618387c37da2c4c445
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-25 19:09:58 +0000

    Add function to calculate average of an array of numbers

--- src/ledger/calculateAverage.js
+++ src/ledger/calculateAverage.js
@@ -2,7 +2,6 @@ function calculateAverage(numbers) {
     if (!Array.isArray(numbers) || numbers.length === 0) {
         return 0;
     }
-    
-    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    const sum = numbers.reduce((total, num) => total + num, 0);
     return sum / numbers.length;
 }
