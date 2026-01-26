9fcaecb117b7436aa4b42ced59d2cbb41248232b
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-26 16:55:56 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -1,31 +1,12 @@
-function fibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 2) return 1;
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
-    return memo[n];
-}
+function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
 
-module.exports = fibonacci;function calculateFibonacci(limit) {
     const sequence = [0, 1];
-    
-    if (limit <= 0) return [];
-    if (limit === 1) return [0];
-    
-    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
-        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
-        sequence.push(nextValue);
+    for (let i = 2; i < n; i++) {
+        const nextTerm = sequence[i - 1] + sequence[i - 2];
+        sequence.push(nextTerm);
     }
-    
     return sequence;
 }
-
-function isFibonacciNumber(num) {
-    if (num < 0) return false;
-    
-    const check1 = Math.sqrt(5 * num * num + 4);
-    const check2 = Math.sqrt(5 * num * num - 4);
-    
-    return Number.isInteger(check1) || Number.isInteger(check2);
-}
-
-module.exports = { calculateFibonacci, isFibonacciNumber };
