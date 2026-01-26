b7a4b713035c4e2723c4130ec7eae1c37d78d566
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-26 02:11:10 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -22,4 +22,46 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+  if (unit.toLowerCase() === 'c') {
+    return {
+      celsius: value,
+      fahrenheit: celsiusToFahrenheit(value),
+      kelvin: value + 273.15
+    };
+  } else if (unit.toLowerCase() === 'f') {
+    const celsius = fahrenheitToCelsius(value);
+    return {
+      celsius: celsius,
+      fahrenheit: value,
+      kelvin: celsius + 273.15
+    };
+  } else if (unit.toLowerCase() === 'k') {
+    const celsius = value - 273.15;
+    return {
+      celsius: celsius,
+      fahrenheit: celsiusToFahrenheit(celsius),
+      kelvin: value
+    };
+  }
+  throw new Error('Invalid temperature unit. Use C, F, or K.');
+}
+
+function formatTemperature(value, decimals = 1) {
+  return Number(value.toFixed(decimals));
+}
+
+export {
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+  convertTemperature,
+  formatTemperature
 };
