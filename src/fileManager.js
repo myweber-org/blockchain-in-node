ce01dc4a6656df92123cd98118b3050ae4235128
InkspillVerse <inkspill.verse@outlook.com>
2026-01-27 02:01:50 +0000

    Add fileManager module with utility functions for file operations

--- src/fileManager.js
+++ src/fileManager.js
@@ -154,4 +154,52 @@ class FileManager {
   }
 }
 
+module.exports = FileManager;const fs = require('fs');
+const path = require('path');
+
+class FileManager {
+  constructor(basePath = '.') {
+    this.basePath = path.resolve(basePath);
+  }
+
+  readFile(filePath) {
+    const fullPath = path.join(this.basePath, filePath);
+    return new Promise((resolve, reject) => {
+      fs.readFile(fullPath, 'utf8', (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  }
+
+  writeFile(filePath, content) {
+    const fullPath = path.join(this.basePath, filePath);
+    return new Promise((resolve, reject) => {
+      fs.writeFile(fullPath, content, 'utf8', (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+
+  listFiles(directory = '.') {
+    const fullPath = path.join(this.basePath, directory);
+    return new Promise((resolve, reject) => {
+      fs.readdir(fullPath, (err, files) => {
+        if (err) reject(err);
+        else resolve(files);
+      });
+    });
+  }
+
+  fileExists(filePath) {
+    const fullPath = path.join(this.basePath, filePath);
+    return new Promise((resolve) => {
+      fs.access(fullPath, fs.constants.F_OK, (err) => {
+        resolve(!err);
+      });
+    });
+  }
+}
+
 module.exports = FileManager;
