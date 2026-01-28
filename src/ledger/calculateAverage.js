dc81a09347b415173eb0df191933edaa64aee723
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-28 14:21:11 +0000

    Add function to calculate the average of an array of numbers

--- src/ledger/calculateAverage.js
+++ src/ledger/calculateAverage.js
@@ -1,7 +1,11 @@
+
 function calculateAverage(numbers) {
     if (!Array.isArray(numbers) || numbers.length === 0) {
         return 0;
     }
+    
     const sum = numbers.reduce((acc, num) => acc + num, 0);
     return sum / numbers.length;
 }
+
+module.exports = calculateAverage;
