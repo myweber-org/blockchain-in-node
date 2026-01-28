852ac429445109aee9ff8ff9b948f8306d448977
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-28 21:28:16 +0000

    Add input validation function for username and password

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -143,4 +143,35 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUserInput, validateUsername, validateEmail };
+export { validateUserInput, validateUsername, validateEmail };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const isUsernameValid = validateUsername(username);
+    const isPasswordValid = validatePassword(password);
+    
+    if (!isUsernameValid && !isPasswordValid) {
+        return 'Invalid username and password';
+    } else if (!isUsernameValid) {
+        return 'Invalid username';
+    } else if (!isPasswordValid) {
+        return 'Invalid password';
+    } else {
+        return 'Valid input';
+    }
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
