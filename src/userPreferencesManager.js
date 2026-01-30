e141788b55a1cd83e0722a986f45d8dbbf3a9c9e
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-30 20:03:53 +0000

    Add user preferences management with localStorage support

--- src/userPreferencesManager.js
+++ src/userPreferencesManager.js
@@ -57,4 +57,50 @@ const userPreferencesManager = {
     }
 };
 
-export default userPreferencesManager;
+export default userPreferencesManager;const userPreferencesManager = (function() {
+    const STORAGE_KEY = 'user_preferences';
+    
+    function getPreferences() {
+        const stored = localStorage.getItem(STORAGE_KEY);
+        return stored ? JSON.parse(stored) : {};
+    }
+    
+    function setPreference(key, value) {
+        const preferences = getPreferences();
+        preferences[key] = value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(preferences));
+        return true;
+    }
+    
+    function getPreference(key, defaultValue = null) {
+        const preferences = getPreferences();
+        return preferences.hasOwnProperty(key) ? preferences[key] : defaultValue;
+    }
+    
+    function removePreference(key) {
+        const preferences = getPreferences();
+        if (preferences.hasOwnProperty(key)) {
+            delete preferences[key];
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(preferences));
+            return true;
+        }
+        return false;
+    }
+    
+    function clearAllPreferences() {
+        localStorage.removeItem(STORAGE_KEY);
+        return true;
+    }
+    
+    function getAllPreferences() {
+        return getPreferences();
+    }
+    
+    return {
+        get: getPreference,
+        set: setPreference,
+        remove: removePreference,
+        clear: clearAllPreferences,
+        getAll: getAllPreferences
+    };
+})();
