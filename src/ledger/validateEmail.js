507be36dda31681ea742977301e12e8870e9b3fb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-30 14:58:22 +0000

    Add email validation utility function

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -56,4 +56,9 @@ module.exports = validateEmail;function validateEmail(email) {
 module.exports = validateEmail;function validateEmail(email) {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
+}function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
+
+module.exports = validateEmail;
