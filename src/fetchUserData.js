fbb1bd1012f7d1e7b0a7973c1da8ca06ec65fd9e
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-31 23:39:08 +0000

    Add fetchUserData function to retrieve and log user information from API

--- src/fetchUserData.js
+++ src/fetchUserData.js
@@ -228,4 +228,25 @@ export { fetchUserData, clearUserCache };function fetchUserData(userId) {
             console.error('Error fetching user data:', error);
             throw error;
         });
+}function fetchUserData(userId) {
+    const apiUrl = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('User Data:', data);
+            console.log('Name:', data.name);
+            console.log('Email:', data.email);
+            console.log('Company:', data.company.name);
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
 }
+
+fetchUserData(1);
