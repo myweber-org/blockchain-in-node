751afae6eb4cd08ecc8573c5498d253f205fd34d
VioletInquiry <violet.inquiry@outlook.com>
2026-02-02 15:29:27 +0000

    Add file encryption utility with AES-256-CBC implementation

--- src/ledger/file-encryption-utility.js
+++ src/ledger/file-encryption-utility.js
@@ -11,93 +11,61 @@ class FileEncryptionUtility {
         return crypto.randomBytes(this.keyLength);
     }
 
-    generateIV() {
-        return crypto.randomBytes(this.ivLength);
-    }
-
-    encryptFile(data, key, iv) {
-        const cipher = crypto.createCipheriv(this.algorithm, key, iv);
-        let encrypted = cipher.update(data, 'utf8', 'hex');
-        encrypted += cipher.final('hex');
-        return {
-            encryptedData: encrypted,
-            iv: iv.toString('hex')
-        };
-    }
-
-    decryptFile(encryptedData, key, iv) {
-        const decipher = crypto.createDecipheriv(this.algorithm, key, Buffer.from(iv, 'hex'));
-        let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
-        decrypted += decipher.final('utf8');
-        return decrypted;
-    }
-
-    hashFile(data) {
-        return crypto.createHash('sha256').update(data).digest('hex');
-    }
-
-    validateKey(key) {
-        if (!Buffer.isBuffer(key)) {
-            throw new Error('Key must be a Buffer');
+    encryptFile(data, key) {
+        if (!Buffer.isBuffer(data)) {
+            data = Buffer.from(data);
         }
-        if (key.length !== this.keyLength) {
-            throw new Error(`Key must be ${this.keyLength} bytes`);
-        }
-        return true;
-    }
-}
 
-module.exports = FileEncryptionUtility;const crypto = require('crypto');
-
-class FileEncryptionUtility {
-    constructor() {
-        this.algorithm = 'aes-256-cbc';
-        this.keyLength = 32;
-        this.ivLength = 16;
-    }
-
-    generateKey() {
-        return crypto.randomBytes(this.keyLength);
-    }
-
-    generateIV() {
-        return crypto.randomBytes(this.ivLength);
-    }
-
-    encryptFile(data, key, iv) {
+        const iv = crypto.randomBytes(this.ivLength);
         const cipher = crypto.createCipheriv(this.algorithm, key, iv);
-        let encrypted = cipher.update(data, 'utf8', 'hex');
-        encrypted += cipher.final('hex');
-        return encrypted;
+        
+        const encrypted = Buffer.concat([
+            cipher.update(data),
+            cipher.final()
+        ]);
+
+        return Buffer.concat([iv, encrypted]);
     }
 
-    decryptFile(encryptedData, key, iv) {
+    decryptFile(encryptedData, key) {
+        if (!Buffer.isBuffer(encryptedData)) {
+            encryptedData = Buffer.from(encryptedData);
+        }
+
+        const iv = encryptedData.slice(0, this.ivLength);
+        const data = encryptedData.slice(this.ivLength);
+        
         const decipher = crypto.createDecipheriv(this.algorithm, key, iv);
-        let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
-        decrypted += decipher.final('utf8');
-        return decrypted;
+        
+        return Buffer.concat([
+            decipher.update(data),
+            decipher.final()
+        ]);
     }
 
-    createKeyFromPassword(password, salt) {
-        return crypto.pbkdf2Sync(password, salt, 100000, this.keyLength, 'sha256');
+    encryptFileWithPassword(data, password) {
+        const salt = crypto.randomBytes(16);
+        const key = crypto.pbkdf2Sync(password, salt, 100000, this.keyLength, 'sha256');
+        
+        const encrypted = this.encryptFile(data, key);
+        return Buffer.concat([salt, encrypted]);
     }
 
-    saveEncryptedFile(filePath, data, key, iv) {
-        const encrypted = this.encryptFile(data, key, iv);
-        const fs = require('fs');
-        const fileData = {
-            iv: iv.toString('hex'),
-            encryptedData: encrypted
-        };
-        fs.writeFileSync(filePath, JSON.stringify(fileData));
-        return true;
+    decryptFileWithPassword(encryptedData, password) {
+        const salt = encryptedData.slice(0, 16);
+        const data = encryptedData.slice(16);
+        
+        const key = crypto.pbkdf2Sync(password, salt, 100000, this.keyLength, 'sha256');
+        return this.decryptFile(data, key);
     }
 
-    loadEncryptedFile(filePath, key) {
-        const fs = require('fs');
-        const fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-        const iv = Buffer.from(fileData.iv, 'hex');
-        return this.decryptFile(fileData.encryptedData, key, iv);
+    calculateHash(data, algorithm = 'sha256') {
+        return crypto.createHash(algorithm).update(data).digest('hex');
+    }
+
+    verifyIntegrity(data, expectedHash, algorithm = 'sha256') {
+        const actualHash = this.calculateHash(data, algorithm);
+        return actualHash === expectedHash;
     }
 }
 
