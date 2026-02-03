a28d48aff38967932c968bf00f3f9d565f74d6be
heyQuim <hey.quim@hotmail.com>
2026-02-03 01:06:53 +0000

    Add utility function to rename files with timestamp prefix

--- src/ledger/fileRenamer.js
+++ src/ledger/fileRenamer.js
@@ -27,4 +27,25 @@ function renameFilesWithSequentialNumbering(fileList, baseName) {
 function getFileExtension(filename) {
     const lastDotIndex = filename.lastIndexOf('.');
     return lastDotIndex === -1 ? '' : filename.substring(lastDotIndex);
+}function addTimestampPrefix(filename) {
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-').slice(0, 19);
+    return `${timestamp}_${filename}`;
 }
+
+function renameFiles(fileList) {
+    if (!Array.isArray(fileList)) {
+        throw new Error('Input must be an array of filenames');
+    }
+    
+    return fileList.map(filename => {
+        if (typeof filename !== 'string') {
+            throw new Error('Each item must be a string');
+        }
+        return addTimestampPrefix(filename);
+    });
+}
+
+module.exports = {
+    addTimestampPrefix,
+    renameFiles
+};
