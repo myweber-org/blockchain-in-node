4cfbfce8f782c496d9cac67d36d87454533e30e8
FelixMakes <felix.makes@hotmail.com>
2026-02-03 23:32:21 +0000

    Add input validation utility for username and password fields

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -3,20 +3,29 @@ function validateUsername(username) {
     return usernameRegex.test(username);
 }
 
-function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
 }
 
-function validateUserInput(username, email) {
+function validateUserInput(username, password) {
     const errors = [];
     
     if (!validateUsername(username)) {
         errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores');
     }
     
-    if (!validateEmail(email)) {
-        errors.push('Please enter a valid email address');
+    if (!validatePassword(password)) {
+        errors.push('Password must be at least 8 characters with uppercase, lowercase, number, and special character');
     }
     
     return {
@@ -25,4 +34,4 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUserInput, validateUsername, validateEmail };
+export { validateUsername, validatePassword, validateUserInput };
