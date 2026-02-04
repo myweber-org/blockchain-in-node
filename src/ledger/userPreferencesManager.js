3f8694006e172e1e8a425b8b5e1a0d0fcebe91af
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-04 00:14:19 +0000

    Add user preferences management with local storage support

--- src/ledger/userPreferencesManager.js
+++ src/ledger/userPreferencesManager.js
@@ -1011,4 +1011,51 @@ if (typeof module !== 'undefined' && module.exports) {
         clear: clearPreferences,
         getAll: getAllPreferences
     };
-})();
+})();const userPreferencesManager = {
+    preferences: {},
+
+    init: function() {
+        const stored = localStorage.getItem('userPreferences');
+        if (stored) {
+            this.preferences = JSON.parse(stored);
+        } else {
+            this.preferences = {
+                theme: 'light',
+                language: 'en',
+                notifications: true,
+                fontSize: 16
+            };
+            this.save();
+        }
+    },
+
+    get: function(key) {
+        return this.preferences[key];
+    },
+
+    set: function(key, value) {
+        this.preferences[key] = value;
+        this.save();
+        return true;
+    },
+
+    save: function() {
+        localStorage.setItem('userPreferences', JSON.stringify(this.preferences));
+    },
+
+    reset: function() {
+        this.preferences = {
+            theme: 'light',
+            language: 'en',
+            notifications: true,
+            fontSize: 16
+        };
+        this.save();
+    },
+
+    getAll: function() {
+        return {...this.preferences};
+    }
+};
+
+userPreferencesManager.init();
