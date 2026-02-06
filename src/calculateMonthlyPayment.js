3d0dee8756ee5df50a013bc3dde0001de7936c8f
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-06 19:17:08 +0000

    Add function to calculate monthly mortgage payment with principal, interest rate, and term

--- src/calculateMonthlyPayment.js
+++ src/calculateMonthlyPayment.js
@@ -19,4 +19,17 @@ module.exports = calculateMonthlyPayment;function calculateMonthlyPayment(princi
         (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
     
     return Math.round(monthlyPayment * 100) / 100;
+}function calculateMonthlyPayment(principal, annualInterestRate, years) {
+    const monthlyInterestRate = annualInterestRate / 12 / 100;
+    const numberOfPayments = years * 12;
+    
+    if (monthlyInterestRate === 0) {
+        return principal / numberOfPayments;
+    }
+    
+    const monthlyPayment = principal * 
+        (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / 
+        (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+    
+    return Math.round(monthlyPayment * 100) / 100;
 }
