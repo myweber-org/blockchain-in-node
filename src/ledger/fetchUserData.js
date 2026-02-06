108561f39f095a64aa341b5b1035997969218076
VioletInquiry <violet.inquiry@outlook.com>
2026-02-06 16:39:54 +0000

    Add fetchUserData utility to retrieve and parse user information from API

--- src/ledger/fetchUserData.js
+++ src/ledger/fetchUserData.js
@@ -92,3 +92,20 @@ function displayUserData(user) {
             throw error;
         });
 }
+function fetchUserData(userId) {
+  return fetch(`https://api.example.com/users/${userId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('User data fetched successfully:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('There was a problem fetching user data:', error);
+      throw error;
+    });
+}
