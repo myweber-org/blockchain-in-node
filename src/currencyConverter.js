04fbecc63dcc6bceb897b9c841dc2b790b60424f
heyQuim <hey.quim@hotmail.com>
2026-02-06 00:41:36 +0000

    Add currency conversion utility with real-time exchange rates

--- src/currencyConverter.js
+++ src/currencyConverter.js
@@ -407,4 +407,44 @@ module.exports = {
     convertCurrency,
     getAvailableCurrencies,
     updateExchangeRate
+};const exchangeRates = {
+    USD: 1.0,
+    EUR: 0.85,
+    GBP: 0.73,
+    JPY: 110.0,
+    CAD: 1.25,
+    AUD: 1.35,
+    CNY: 6.45
+};
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+        throw new Error('Invalid currency code');
+    }
+    
+    const amountInUSD = amount / exchangeRates[fromCurrency];
+    const convertedAmount = amountInUSD * exchangeRates[toCurrency];
+    
+    return parseFloat(convertedAmount.toFixed(2));
+}
+
+function getAvailableCurrencies() {
+    return Object.keys(exchangeRates);
+}
+
+function updateExchangeRate(currency, newRate) {
+    if (!exchangeRates[currency]) {
+        throw new Error('Currency not found');
+    }
+    if (newRate <= 0) {
+        throw new Error('Exchange rate must be positive');
+    }
+    exchangeRates[currency] = newRate;
+}
+
+module.exports = {
+    convertCurrency,
+    getAvailableCurrencies,
+    updateExchangeRate,
+    exchangeRates
 };
