872dd512ff9d90bd36f5918f56006eef342ab23a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-07 19:21:57 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -2,9 +2,9 @@ function calculateFibonacci(n) {
     if (n <= 0) return [];
     if (n === 1) return [0];
     
-    const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    let sequence = [0, 1];
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= n) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
 }
