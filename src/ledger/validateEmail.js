3153aa09399ddae62c9680f9740b8c3c4bde7cbf
FelixMakes <felix.makes@hotmail.com>
2026-02-07 01:50:51 +0000

    Add email validation utility function with regex pattern

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -35,4 +35,9 @@ module.exports = validateEmail;function validateEmail(email) {
 module.exports = validateEmail;function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
+}function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
+
+module.exports = validateEmail;
