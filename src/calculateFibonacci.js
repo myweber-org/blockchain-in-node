b262cc3e2b2937ae90373a1c764ee4fe94556fa2
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-08 15:54:57 +0000

    Add function to generate Fibonacci sequence up to a given number

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -7,4 +7,10 @@ function calculateFibonacci(n) {
         sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
+}function calculateFibonacci(limit) {
+    const sequence = [0, 1];
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    }
+    return sequence;
 }
