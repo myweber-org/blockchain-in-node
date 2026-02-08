cef120baeab75b2b4fbaf9b6849e2275b5743d93
FelixMakes <felix.makes@hotmail.com>
2026-02-08 23:05:37 +0000

    Add email validation utility function

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -1,4 +1,9 @@
 function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
+}function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
 }
+
+module.exports = validateEmail;
