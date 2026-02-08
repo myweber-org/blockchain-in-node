73d1318a86494d877840da32fe442c9329c0895f
FelixMakes <felix.makes@hotmail.com>
2026-02-08 23:38:03 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -85,4 +85,9 @@ module.exports = validateEmail;function validateEmail(email) {
     return emailRegex.test(email);
 }
 
+module.exports = validateEmail;function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
 module.exports = validateEmail;
