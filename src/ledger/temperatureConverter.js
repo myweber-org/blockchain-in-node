106e9dfeb6415c32b6141c9fcc050d7e735e75a5
VioletInquiry <violet.inquiry@outlook.com>
2026-02-10 14:49:02 +0000

    Add utility function to convert between Celsius and Fahrenheit

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -92,4 +92,26 @@ module.exports = {
     fahrenheitToCelsius,
     convertTemperature,
     formatTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+    if (unit.toLowerCase() === 'c') {
+        return celsiusToFahrenheit(value);
+    } else if (unit.toLowerCase() === 'f') {
+        return fahrenheitToCelsius(value);
+    } else {
+        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
+    }
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius,
+    convertTemperature
 };
