7e02af9ef52b5f13ac0d27db8deffe24842aca22
InkspillVerse <inkspill.verse@outlook.com>
2026-02-10 15:31:50 +0000

    Add input validation function to sanitize user-provided strings

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -1,6 +1,14 @@
-function validateUsername(username) {
-    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-    return usernameRegex.test(username);
+function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;');
 }
 
 function validateEmail(email) {
@@ -8,21 +16,22 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-function validateUserInput(username, email) {
-    const errors = [];
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     
-    if (!validateUsername(username)) {
-        errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores');
-    }
-    
-    if (!validateEmail(email)) {
-        errors.push('Please enter a valid email address');
-    }
-    
-    return {
-        isValid: errors.length === 0,
-        errors: errors
-    };
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
 }
 
-module.exports = validateUserInput;
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
