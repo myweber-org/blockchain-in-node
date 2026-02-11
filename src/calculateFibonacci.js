35e8c3be69cc73682adda56c6fa759b9fc9383c6
ElaraWrites <elara.writes@outlook.com>
2026-02-11 22:08:17 +0000

    Implement a function to generate Fibonacci sequence up to a given number of terms

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -1,35 +1,9 @@
-function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    
-    let sequence = [0, 1];
-    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= n) {
-        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
-    }
-    return sequence;
-}function calculateFibonacci(limit) {
+function generateFibonacci(terms) {
+    if (terms <= 0) return [];
+    if (terms === 1) return [0];
     const sequence = [0, 1];
-    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
-        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
-    }
-    return sequence;
-}function fibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 2) return 1;
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
-    return memo[n];
-}
-
-function printFibonacciSequence(limit) {
-    for (let i = 1; i <= limit; i++) {
-        console.log(`Fibonacci(${i}) = ${fibonacci(i)}`);
-    }
-}
-
-module.exports = { fibonacci, printFibonacciSequence };function calculateFibonacci(limit) {
-    const sequence = [0, 1];
-    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
-        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    for (let i = 2; i < terms; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
     }
     return sequence;
 }
