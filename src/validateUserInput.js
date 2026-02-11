f5710bea044b0d94e3032eff0a6cb582f99a72c6
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-11 15:08:21 +0000

    Add input validation for username and password fields

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -64,4 +64,17 @@ module.exports = validateUserInput;function validateUserInput(username, password
         isValid: errors.length === 0,
         errors: errors
     };
+}function validateUserInput(username, password) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/;
+
+    if (!usernameRegex.test(username)) {
+        throw new Error('Invalid username format');
+    }
+
+    if (!passwordRegex.test(password)) {
+        throw new Error('Password must be at least 8 characters with letters and numbers');
+    }
+
+    return true;
 }
