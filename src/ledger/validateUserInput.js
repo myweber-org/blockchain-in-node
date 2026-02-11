ff7f3ed196b0deefe2061cdc0fdf779e1431a03e
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-11 20:16:59 +0000

    Add input validation for username and password fields

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -207,4 +207,29 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUserInput, validateUsername, validateEmail };
+export { validateUserInput, validateUsername, validateEmail };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const isUsernameValid = validateUsername(username);
+    const isPasswordValid = validatePassword(password);
+    
+    if (!isUsernameValid) {
+        throw new Error('Invalid username format');
+    }
+    
+    if (!isPasswordValid) {
+        throw new Error('Invalid password format');
+    }
+    
+    return true;
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
