a2251e52352755e46214dcf8642ce1e3e18ab557
FelixMakes <felix.makes@hotmail.com>
2026-02-11 15:49:50 +0000

    Add user preferences management with local storage support

--- src/userPreferencesManager.js
+++ src/userPreferencesManager.js
@@ -912,4 +912,38 @@ if (typeof module !== 'undefined' && module.exports) {
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = UserPreferencesManager;
-}
+}const UserPreferences = {
+    preferences: {},
+
+    init: function() {
+        const stored = localStorage.getItem('userPreferences');
+        if (stored) {
+            this.preferences = JSON.parse(stored);
+        }
+    },
+
+    setPreference: function(key, value) {
+        this.preferences[key] = value;
+        this.save();
+    },
+
+    getPreference: function(key, defaultValue = null) {
+        return this.preferences[key] || defaultValue;
+    },
+
+    removePreference: function(key) {
+        delete this.preferences[key];
+        this.save();
+    },
+
+    save: function() {
+        localStorage.setItem('userPreferences', JSON.stringify(this.preferences));
+    },
+
+    clearAll: function() {
+        this.preferences = {};
+        localStorage.removeItem('userPreferences');
+    }
+};
+
+UserPreferences.init();
