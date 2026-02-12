335a0fe2e5e47f2e1c9d910f6ff8303ec0c3e61b
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-12 23:34:20 +0000

    Add email validation utility with regex pattern

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -6,4 +6,9 @@ function validateEmail(email) {
     return pattern.test(email);
 }
 
+module.exports = validateEmail;function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 module.exports = validateEmail;
