d63cbb22e4b732a130ba8816a1f4ad3cd294438d
VioletInquiry <violet.inquiry@outlook.com>
2026-02-13 01:38:27 +0000

    Add function to generate Fibonacci sequence up to a given number

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -9,4 +9,10 @@ function calculateFibonacci(limit) {
         sequence.push(nextValue);
     }
     return sequence;
+}function calculateFibonacci(limit) {
+    const sequence = [0, 1];
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    }
+    return sequence;
 }
