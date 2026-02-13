3745e60b6790534ee59ccced03597fb3555868ff
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-13 15:29:51 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -15,4 +15,13 @@ function calculateFibonacci(limit) {
         sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
+}function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    const sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
 }
