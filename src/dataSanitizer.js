a65ed991f911d3cc9c2c35cd615d15a8f8733147
heyQuim <hey.quim@hotmail.com>
2026-02-13 22:18:31 +0000

    Add data sanitization utility to prevent XSS attacks

--- src/dataSanitizer.js
+++ src/dataSanitizer.js
@@ -125,4 +125,28 @@ function validateAndSanitizeUserInput(userInput) {
     return sanitized;
 }
 
-export { validateAndSanitizeUserInput };
+export { validateAndSanitizeUserInput };function sanitizeInput(input) {
+  if (typeof input !== 'string') return '';
+  
+  const div = document.createElement('div');
+  div.textContent = input;
+  return div.innerHTML;
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+function escapeHtml(text) {
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+export { sanitizeInput, validateEmail, escapeHtml };
