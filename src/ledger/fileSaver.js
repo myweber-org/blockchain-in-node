d15ff65c55142d1ad21e827c0a8c0130644ec3b4
FelixMakes <felix.makes@hotmail.com>
2026-02-14 22:36:55 +0000

    Add utility function to save JSON data as a file

--- src/ledger/fileSaver.js
+++ src/ledger/fileSaver.js
@@ -29,4 +29,15 @@ function saveTextAsFile(textContent, fileName) {
     link.click();
     document.body.removeChild(link);
     URL.revokeObjectURL(url);
+}function saveJSONToFile(data, filename) {
+    const jsonString = JSON.stringify(data, null, 2);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename || 'data.json';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
 }
