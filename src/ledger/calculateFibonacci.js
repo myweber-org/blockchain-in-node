be406f8eb88cd15d8908fc5278527228c1634c51
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-15 19:39:41 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -1,49 +1,10 @@
-function calculateFibonacci(limit) {
-    if (limit <= 0) return [];
-    if (limit === 1) return [0];
-    
-    const sequence = [0, 1];
-    while (true) {
-        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
-        if (nextValue > limit) break;
-        sequence.push(nextValue);
-    }
-    return sequence;
-}function calculateFibonacci(limit) {
-    const sequence = [0, 1];
-    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
-        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
-    }
-    return sequence;
-}function calculateFibonacci(n) {
+function calculateFibonacci(n) {
     if (n <= 0) return [];
     if (n === 1) return [0];
     
-    const sequence = [0, 1];
+    let fib = [0, 1];
     for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
+        fib.push(fib[i - 1] + fib[i - 2]);
     }
-    return sequence;
-}function calculateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    
-    const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
-    }
-    return sequence;
-}function calculateFibonacci(limit) {
-    const sequence = [0, 1];
-    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
-        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
-    }
-    return sequence;
-}function fibonacci(n, memo = {}) {
-    if (n in memo) return memo[n];
-    if (n <= 1) return n;
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
-    return memo[n];
+    return fib;
 }
-
-console.log(fibonacci(10));
