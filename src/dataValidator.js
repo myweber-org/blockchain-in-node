6c5daa7f4f4cf305c3c4cd6101d09d31809328d8
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-15 14:53:43 +0000

    Add data validation utility for user input sanitization

--- src/dataValidator.js
+++ src/dataValidator.js
@@ -67,4 +67,26 @@ function validateFormData(userData) {
   };
 }
 
-export { validateEmail, validatePhoneNumber, validateFormData };
+export { validateEmail, validatePhoneNumber, validateFormData };function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 8 && 
+           /[A-Z]/.test(password) && 
+           /[a-z]/.test(password) && 
+           /\d/.test(password);
+}
+
+function sanitizeInput(input) {
+    return input.trim()
+                .replace(/[<>]/g, '')
+                .substring(0, 255);
+}
+
+module.exports = {
+    validateEmail,
+    validatePassword,
+    sanitizeInput
+};
