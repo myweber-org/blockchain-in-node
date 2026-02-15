cf9d68abcdc9fdffcbd16401e2f2d90ce81182d9
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-15 22:47:59 +0000

    Add input validation for username and password fields

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -153,4 +153,54 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUserInput, validateUsername, validateEmail };
+export { validateUserInput, validateUsername, validateEmail };function validateUsername(username) {
+  const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+  return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+  const minLength = 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  
+  return password.length >= minLength && 
+         hasUpperCase && 
+         hasLowerCase && 
+         hasNumbers && 
+         hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+  const usernameValid = validateUsername(username);
+  const passwordValid = validatePassword(password);
+  
+  if (!usernameValid && !passwordValid) {
+    return { 
+      valid: false, 
+      message: 'Username must be 3-20 alphanumeric characters and password must be at least 8 characters with uppercase, lowercase, number, and special character' 
+    };
+  }
+  
+  if (!usernameValid) {
+    return { 
+      valid: false, 
+      message: 'Username must be 3-20 alphanumeric characters' 
+    };
+  }
+  
+  if (!passwordValid) {
+    return { 
+      valid: false, 
+      message: 'Password must be at least 8 characters with uppercase, lowercase, number, and special character' 
+    };
+  }
+  
+  return { 
+    valid: true, 
+    message: 'User input is valid' 
+  };
+}
+
+export { validateUserInput, validateUsername, validatePassword };
