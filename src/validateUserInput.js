0e39887522371c0e1a9dc301084945eaa06d87ae
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-16 21:57:12 +0000

    Add input validation function to sanitize user-provided strings

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -92,4 +92,37 @@ module.exports = validateUserInput;function validateUserInput(username, password
     }
     
     return { valid: true, message: "Input is valid." };
+}function sanitizeInput(input) {
+  if (typeof input !== 'string') {
+    return '';
+  }
+  
+  return input
+    .trim()
+    .replace(/[<>]/g, '')
+    .substring(0, 255);
 }
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false;
+  }
+  
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+  
+  return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+module.exports = {
+  sanitizeInput,
+  validateEmail,
+  validatePassword
+};
