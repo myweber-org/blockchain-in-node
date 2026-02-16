1c7b5ac56c0013047f937d2b80afcf7b9f41d0f7
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-16 15:43:18 +0000

    Add utility function to convert Celsius to Fahrenheit

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -69,4 +69,15 @@ export {
     kelvinToFahrenheit,
     convertTemperature,
     formatTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius
 };
