f30b66c04062b4d06ee702190fe24149131b3ee1
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-16 15:57:55 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -1,14 +1,4 @@
 function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-}function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
 }
-
-module.exports = validateEmail;function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
-module.exports = validateEmail;
