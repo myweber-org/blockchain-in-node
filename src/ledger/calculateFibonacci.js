5f22bab0b1b6eeac0a3cfc1fd53365b0d9f14045
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-17 20:02:27 +0000

    Add function to generate Fibonacci sequence up to a given number

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -4,4 +4,10 @@ function calculateFibonacci(n, memo = {}) {
     if (n <= 1) return n;
     memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
     return memo[n];
+}function calculateFibonacci(limit) {
+    const sequence = [0, 1];
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    }
+    return sequence;
 }
