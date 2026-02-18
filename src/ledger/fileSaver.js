b7f0c4d42453ccb9daf577aee15f3883bc533f79
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-18 20:44:26 +0000

    Add utility function to save text content as a file with a given filename

--- src/ledger/fileSaver.js
+++ src/ledger/fileSaver.js
@@ -7,37 +7,4 @@ function saveTextAsFile(textContent, fileName) {
     link.click();
     document.body.removeChild(link);
     URL.revokeObjectURL(link.href);
-}function saveData(data, filename, type) {
-    const blob = new Blob([data], { type: type });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-    setTimeout(() => {
-        document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
-    }, 100);
-}function saveTextAsFile(content, filename) {
-    const blob = new Blob([content], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
-}function saveJSONToFile(data, filename) {
-    const jsonString = JSON.stringify(data, null, 2);
-    const blob = new Blob([jsonString], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = filename || 'data.json';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
 }
