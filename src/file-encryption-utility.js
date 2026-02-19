a09d1816e16bf2cf517bcba5a40fee9062fb43d0
InkspillVerse <inkspill.verse@outlook.com>
2026-02-19 14:57:32 +0000

    Add file encryption utility with AES-256-CBC

--- src/file-encryption-utility.js
+++ src/file-encryption-utility.js
@@ -41,4 +41,47 @@ class FileEncryptionUtility {
     }
 }
 
+module.exports = FileEncryptionUtility;const crypto = require('crypto');
+
+class FileEncryptionUtility {
+    constructor() {
+        this.algorithm = 'aes-256-cbc';
+        this.keyLength = 32;
+        this.ivLength = 16;
+    }
+
+    generateKey() {
+        return crypto.randomBytes(this.keyLength);
+    }
+
+    generateIV() {
+        return crypto.randomBytes(this.ivLength);
+    }
+
+    encryptFile(data, key, iv) {
+        const cipher = crypto.createCipheriv(this.algorithm, key, iv);
+        let encrypted = cipher.update(data, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        return {
+            encryptedData: encrypted,
+            iv: iv.toString('hex')
+        };
+    }
+
+    decryptFile(encryptedData, key, iv) {
+        const decipher = crypto.createDecipheriv(this.algorithm, key, Buffer.from(iv, 'hex'));
+        let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+        decrypted += decipher.final('utf8');
+        return decrypted;
+    }
+
+    createKeyFromPassword(password, salt) {
+        return crypto.pbkdf2Sync(password, salt, 100000, this.keyLength, 'sha256');
+    }
+
+    validateKey(key) {
+        return Buffer.isBuffer(key) && key.length === this.keyLength;
+    }
+}
+
 module.exports = FileEncryptionUtility;
