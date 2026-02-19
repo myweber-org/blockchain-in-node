cd15c1c9174e0a9a27464a16928b79a7d94163e3
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-19 18:32:41 +0000

    Add function to calculate Fibonacci sequence up to a given number

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -39,4 +39,15 @@ module.exports = {
         sequence.push(next);
     }
     return sequence;
+}function calculateFibonacci(limit) {
+    if (limit <= 0) return [];
+    if (limit === 1) return [0];
+
+    const sequence = [0, 1];
+    while (true) {
+        const next = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+        if (next > limit) break;
+        sequence.push(next);
+    }
+    return sequence;
 }
