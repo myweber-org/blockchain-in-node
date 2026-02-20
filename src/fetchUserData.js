e0cbe392cc1586a1cbe1b804df65b493062d733c
FelixMakes <felix.makes@hotmail.com>
2026-02-20 14:36:49 +0000

    Add fetchUserData function to retrieve and process user data from API

--- src/fetchUserData.js
+++ src/fetchUserData.js
@@ -167,4 +167,22 @@ function displayUserData(user) {
             <p>Location: ${user.location}</p>
         `;
     }
+}function fetchUserData(userId) {
+    const apiUrl = `https://api.example.com/users/${userId}`;
+    
+    return fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('User data fetched successfully:', data);
+            return data;
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+            throw error;
+        });
 }
