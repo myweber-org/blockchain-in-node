8f155d6d535c42246c54042405d6f482c0b66210
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-20 20:18:14 +0000

    Add temperature conversion utility with Celsius to Fahrenheit and vice versa

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -47,4 +47,33 @@ module.exports = {
     celsiusToFahrenheit,
     fahrenheitToCelsius,
     convertTemperature
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function convertTemperature(value, unit) {
+    if (unit === 'C') {
+        return celsiusToFahrenheit(value);
+    } else if (unit === 'F') {
+        return fahrenheitToCelsius(value);
+    } else {
+        throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
+    }
+}
+
+function formatTemperature(value, unit) {
+    const converted = convertTemperature(value, unit);
+    const targetUnit = unit === 'C' ? 'F' : 'C';
+    return `${value}°${unit} = ${converted.toFixed(1)}°${targetUnit}`;
+}
+
+module.exports = {
+    celsiusToFahrenheit,
+    fahrenheitToCelsius,
+    convertTemperature,
+    formatTemperature
 };
