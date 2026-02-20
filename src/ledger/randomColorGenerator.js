9ddd453613697cd5c479a15091e4a9dfa466558a
heyQuim <hey.quim@hotmail.com>
2026-02-20 01:19:37 +0000

    Add random color generator utility function

--- src/ledger/randomColorGenerator.js
+++ src/ledger/randomColorGenerator.js
@@ -15,4 +15,21 @@ function generateRandomColors(count) {
   return colors;
 }
 
-module.exports = { getRandomColor, generateRandomColors };
+module.exports = { getRandomColor, generateRandomColors };function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function generateRandomColors(count) {
+  const colors = [];
+  for (let i = 0; i < count; i++) {
+    colors.push(getRandomColor());
+  }
+  return colors;
+}
+
+export { getRandomColor, generateRandomColors };
