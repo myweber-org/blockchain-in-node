ca4148bfbea4a7370cbb8648ad4c0cddb896ad95
VioletInquiry <violet.inquiry@outlook.com>
2026-02-21 22:07:54 +0000

    Add function to calculate average of an array of numbers

--- src/ledger/calculateAverage.js
+++ src/ledger/calculateAverage.js
@@ -42,4 +42,11 @@ module.exports = calculateAverage;function calculateAverage(numbers) {
     }, 0);
     
     return sum / numbers.length;
+}function calculateAverage(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return 0;
+    }
+    
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    return sum / numbers.length;
 }
