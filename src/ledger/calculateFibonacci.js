797554f078266becab461f442a5346dbb6e77e9c
InkspillVerse <inkspill.verse@outlook.com>
2026-02-21 14:50:32 +0000

    Add function to generate Fibonacci sequence up to a given number

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -50,4 +50,10 @@ module.exports = {
         sequence.push(next);
     }
     return sequence;
+}function calculateFibonacci(limit) {
+    const sequence = [0, 1];
+    while (sequence[sequence.length - 1] + sequence[sequence.length - 2] <= limit) {
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    }
+    return sequence;
 }
