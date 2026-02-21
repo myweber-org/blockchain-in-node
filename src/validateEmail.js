cb6c9fad2219837788adb03a33b67d5a79a4d960
VioletInquiry <violet.inquiry@outlook.com>
2026-02-21 22:38:46 +0000

    Add email validation utility with regex pattern

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -24,4 +24,7 @@ module.exports = validateEmail;function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-module.exports = validateEmail;
+module.exports = validateEmail;function validateEmail(email) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+}
