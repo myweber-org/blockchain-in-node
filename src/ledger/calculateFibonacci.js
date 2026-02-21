30ffe53ea1f1da169f7c87ea03ff06210ddfae4c
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-21 20:05:07 +0000

    Add function to compute nth Fibonacci number using memoization

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -56,4 +56,17 @@ module.exports = {
         sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
+}function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
+
+function printFibonacciSequence(limit) {
+    for (let i = 1; i <= limit; i++) {
+        console.log(`Fibonacci(${i}) = ${fibonacci(i)}`);
+    }
+}
+
+module.exports = { fibonacci, printFibonacciSequence };
