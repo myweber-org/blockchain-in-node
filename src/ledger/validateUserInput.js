32d1d9976f8dcc2a4f3b8026c0a88f7f32f0bb14
FelixMakes <felix.makes@hotmail.com>
2026-02-21 23:35:50 +0000

    Add input validation utility for username and password fields

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -204,3 +204,40 @@ function validateUserInput(username, password) {
 }
 
 export { validateUserInput, validateUsername, validatePassword };
+function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    
+    return password.length >= minLength && 
+           hasUpperCase && 
+           hasLowerCase && 
+           hasNumbers && 
+           hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const errors = [];
+    
+    if (!validateUsername(username)) {
+        errors.push('Username must be 3-20 characters and contain only letters, numbers, and underscores');
+    }
+    
+    if (!validatePassword(password)) {
+        errors.push('Password must be at least 8 characters and contain uppercase, lowercase, numbers, and special characters');
+    }
+    
+    return {
+        isValid: errors.length === 0,
+        errors: errors
+    };
+}
+
+export { validateUsername, validatePassword, validateUserInput };
