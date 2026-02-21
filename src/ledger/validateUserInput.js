d7a248d1db3ef34ff254541e616303253c02e8a1
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-21 18:38:04 +0000

    Add input validation function for username and password

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -85,4 +85,38 @@ export { validateUserInput, validateUsername, validateEmail };function validateU
         isValid: true,
         message: "Input validation successful."
     };
+}function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
 }
+
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return 'Invalid username and password';
+    } else if (!usernameValid) {
+        return 'Invalid username';
+    } else if (!passwordValid) {
+        return 'Invalid password';
+    }
+    return 'Valid input';
+}
+
+module.exports = {
+    validateUsername,
+    validatePassword,
+    validateUserInput
+};
