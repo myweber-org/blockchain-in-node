13fb0a2f21eace54c57e4447feb249c238a5af99
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-22 19:39:11 +0000

    Add email validation utility function with regex pattern

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -19,4 +19,9 @@ module.exports = validateEmail;function validateEmail(email) {
     return emailRegex.test(email);
 }
 
+module.exports = validateEmail;function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
 module.exports = validateEmail;
