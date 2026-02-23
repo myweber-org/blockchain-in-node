b4458c58dbd83d8d7ae9535b05afc6e1902cc9d4
FelixMakes <felix.makes@hotmail.com>
2026-02-23 22:29:27 +0000

    Add random color generator utility function

--- src/ledger/randomColorGenerator.js
+++ src/ledger/randomColorGenerator.js
@@ -55,4 +55,27 @@ function generateRandomHSLColor() {
     return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
-export { generateRandomColor, generateRandomRGBColor, generateRandomHSLColor };
+export { generateRandomColor, generateRandomRGBColor, generateRandomHSLColor };function generateRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function generateRandomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function generateRandomRGBA(alpha = 1) {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+export { generateRandomColor, generateRandomRGB, generateRandomRGBA };
