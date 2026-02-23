8d646843166119b5b719a1f2bd0e4ab5c873a030
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-23 19:55:46 +0000

    Add random color generator utility function

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -62,4 +62,27 @@ function generateRandomRGBA(alpha = Math.random().toFixed(2)) {
   return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
 
-export { generateRandomColor, generateRandomRGB, generateRandomRGBA };
+export { generateRandomColor, generateRandomRGB, generateRandomRGBA };function generateRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function generateRandomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function generateRandomHSL() {
+  const h = Math.floor(Math.random() * 360);
+  const s = Math.floor(Math.random() * 101);
+  const l = Math.floor(Math.random() * 101);
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+export { generateRandomColor, generateRandomRGB, generateRandomHSL };
