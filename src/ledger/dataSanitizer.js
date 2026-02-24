5dd33b9eab8955194efe73a8f9dac8c189547f4e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-24 23:04:32 +0000

    Add data sanitization utility to prevent XSS attacks

--- src/ledger/dataSanitizer.js
+++ src/ledger/dataSanitizer.js
@@ -131,4 +131,27 @@ function validateAndSanitize(userInput) {
     return sanitized.length > maxLength ? sanitized.substring(0, maxLength) : sanitized;
 }
 
-export { validateAndSanitize };
+export { validateAndSanitize };function sanitizeInput(input) {
+    if (typeof input !== 'string') return '';
+    const div = document.createElement('div');
+    div.textContent = input;
+    return div.innerHTML;
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function escapeHtml(text) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+export { sanitizeInput, validateEmail, escapeHtml };
