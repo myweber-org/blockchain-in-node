89522144b742707ecaf6468afebf0c6b52494d6c
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-24 19:00:43 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -32,4 +32,9 @@ module.exports = validateEmail;function validateEmail(email) {
 module.exports = validateEmail;function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
+}function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
+
+module.exports = validateEmail;
