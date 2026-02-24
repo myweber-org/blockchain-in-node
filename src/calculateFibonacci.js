6b465d259349535038325fd25b6a6cadf1183b36
FelixMakes <felix.makes@hotmail.com>
2026-02-24 15:01:12 +0000

    Implement a function to compute the nth Fibonacci number using memoization

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -97,4 +97,22 @@ module.exports = {
         sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     }
     return sequence;
+}function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 1) return n;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
+
+function printFibonacciSequence(limit) {
+    for (let i = 0; i <= limit; i++) {
+        console.log(`Fibonacci(${i}) = ${fibonacci(i)}`);
+    }
+}
+
+if (require.main === module) {
+    const limit = 10;
+    printFibonacciSequence(limit);
+}
+
+module.exports = { fibonacci, printFibonacciSequence };
