a12089e85ade2e393b5cc5f41af1f10cbe6b5330
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-25 18:56:49 +0000

    Add function to calculate the average of an array of numbers

--- src/calculateAverage.js
+++ src/calculateAverage.js
@@ -4,29 +4,4 @@ function calculateAverage(numbers) {
     }
     const sum = numbers.reduce((acc, num) => acc + num, 0);
     return sum / numbers.length;
-}function calculateAverage(numbers) {
-    if (!Array.isArray(numbers) || numbers.length === 0) {
-        return 0;
-    }
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-    return sum / numbers.length;
-}function calculateAverage(numbers) {
-    if (!Array.isArray(numbers) || numbers.length === 0) {
-        return 0;
-    }
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-    return sum / numbers.length;
-}function calculateAverage(numbers) {
-    if (!Array.isArray(numbers) || numbers.length === 0) {
-        return 0;
-    }
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-    return sum / numbers.length;
-}function calculateAverage(numbers) {
-    if (!Array.isArray(numbers) || numbers.length === 0) {
-        return 0;
-    }
-    
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-    return sum / numbers.length;
 }
