46a5ddec397155ffbc21506f29f41d2696b6b6a4
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-25 19:53:57 +0000

    Add input validation function for username and password

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -335,4 +335,29 @@ function validateUserInput(username, password) {
     return { valid: true, message: 'Input validation successful' };
 }
 
-module.exports = { validateUserInput, validateUsername, validatePassword };
+module.exports = { validateUserInput, validateUsername, validatePassword };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return { valid: false, message: 'Invalid username and password format' };
+    } else if (!usernameValid) {
+        return { valid: false, message: 'Invalid username format' };
+    } else if (!passwordValid) {
+        return { valid: false, message: 'Invalid password format' };
+    }
+    
+    return { valid: true, message: 'Validation successful' };
+}
+
+export { validateUserInput, validateUsername, validatePassword };
