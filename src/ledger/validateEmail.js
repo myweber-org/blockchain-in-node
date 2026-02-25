a65c4095110ba50148f3d1bd50629306cba050ba
heyQuim <hey.quim@hotmail.com>
2026-02-25 21:55:41 +0000

    Add email validation utility function with regex pattern

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -1,19 +1,4 @@
 function validateEmail(email) {
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return pattern.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
-
-module.exports = validateEmail;function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
-module.exports = validateEmail;
