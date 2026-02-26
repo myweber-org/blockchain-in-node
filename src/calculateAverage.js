a31447e8108d225eeff2365cc1c7b3b2e80a052d
VioletInquiry <violet.inquiry@outlook.com>
2026-02-26 21:36:27 +0000

    Add function to calculate average of an array of numbers

--- src/calculateAverage.js
+++ src/calculateAverage.js
@@ -10,4 +10,10 @@ function calculateAverage(numbers) {
     }
     const sum = numbers.reduce((acc, num) => acc + num, 0);
     return sum / numbers.length;
+}function calculateAverage(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return 0;
+    }
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    return sum / numbers.length;
 }
