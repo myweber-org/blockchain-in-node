ae14cc86a9e0b023a7a6bf4cf9c12907af80621c
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-26 19:46:09 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -74,4 +74,15 @@ module.exports = { fibonacci, printFibonacciSequence };function calculateFibonac
     if (n <= 1) return n;
     memo[n] = calculateFibonacci(n - 1, memo) + calculateFibonacci(n - 2, memo);
     return memo[n];
+}function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    return fib;
 }
+
+console.log(calculateFibonacci(10));
