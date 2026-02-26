3dd96168942f470f82b0ffabd6832b34d9c478f9
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-26 19:08:18 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -3,4 +3,7 @@ function validateEmail(email) {
     return emailRegex.test(email);
 }
 
-module.exports = validateEmail;
+module.exports = validateEmail;function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
