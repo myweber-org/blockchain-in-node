93e129721e5871bec665d13a0478c88fa83797d1
ElaraWrites <elara.writes@outlook.com>
2026-02-26 21:26:01 +0000

    Add email validation utility function

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -13,4 +13,9 @@ module.exports = validateEmail;function validateEmail(email) {
   return emailRegex.test(email);
 }
 
+module.exports = validateEmail;function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
 module.exports = validateEmail;
