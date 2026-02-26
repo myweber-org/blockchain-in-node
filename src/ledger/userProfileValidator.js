7dcb47077cae018c4247fde2cb1c6c9e898a133c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-26 21:01:12 +0000

    Add input validation for user profile form fields

--- src/ledger/userProfileValidator.js
+++ src/ledger/userProfileValidator.js
@@ -95,4 +95,43 @@ module.exports = validateUserProfile;function validateUserProfile(profile) {
     };
 }
 
-module.exports = validateUserProfile;
+module.exports = validateUserProfile;function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validateUsername(username) {
+    return username.length >= 3 && username.length <= 20;
+}
+
+function validatePassword(password) {
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*]/.test(password);
+    
+    return password.length >= 8 && hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+}
+
+function validateProfileForm(userData) {
+    const errors = {};
+    
+    if (!validateEmail(userData.email)) {
+        errors.email = 'Invalid email format';
+    }
+    
+    if (!validateUsername(userData.username)) {
+        errors.username = 'Username must be between 3 and 20 characters';
+    }
+    
+    if (!validatePassword(userData.password)) {
+        errors.password = 'Password must be at least 8 characters with uppercase, lowercase, number and special character';
+    }
+    
+    return {
+        isValid: Object.keys(errors).length === 0,
+        errors: errors
+    };
+}
+
+export { validateProfileForm, validateEmail, validateUsername, validatePassword };
