e60b6001f3f8c8ade56cc472677e4b426485087a
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-26 16:48:55 +0000

    Add email validation utility with regex pattern

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -1,19 +1,4 @@
 function validateEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
-
-module.exports = validateEmail;function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
