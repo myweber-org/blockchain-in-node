78bd2c5b826b17666f5ae26020f86296829effc3
ElaraWrites <elara.writes@outlook.com>
2026-02-27 16:29:16 +0000

    Add input validation function to sanitize user-provided strings

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -151,4 +151,37 @@ function validateUserInput(username, email) {
     };
 }
 
-export { validateUserInput, validateUsername, validateEmail };
+export { validateUserInput, validateUsername, validateEmail };function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    
+    return input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;')
+        .trim()
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(sanitizeInput(email));
+}
+
+function validatePassword(password) {
+    const sanitizedPassword = sanitizeInput(password);
+    return sanitizedPassword.length >= 8 && 
+           /[A-Z]/.test(sanitizedPassword) && 
+           /[a-z]/.test(sanitizedPassword) && 
+           /[0-9]/.test(sanitizedPassword);
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
