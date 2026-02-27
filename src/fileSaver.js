4a0410f0083123703027c34c716d471edde8f806
heyQuim <hey.quim@hotmail.com>
2026-02-27 14:32:55 +0000

    Add utility function to save text content as a file with a specified filename

--- src/fileSaver.js
+++ src/fileSaver.js
@@ -1,24 +1,4 @@
-function saveJSONToFile(data, filename) {
-    const jsonStr = JSON.stringify(data, null, 2);
-    const blob = new Blob([jsonStr], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = filename.endsWith('.json') ? filename : filename + '.json';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-}function saveTextAsFile(content, filename) {
-    const blob = new Blob([content], { type: 'text/plain' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(link.href);
-}function saveTextAsFile(textContent, fileName) {
+function saveTextAsFile(textContent, fileName) {
     const blob = new Blob([textContent], { type: 'text/plain' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
