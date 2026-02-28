bb4fe981fe3021adc4730497b7bb17526aba5aed
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-28 19:08:57 +0000

    Add input validation function to sanitize user-provided strings

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -53,4 +53,29 @@ function validateUserInput(username, password) {
     return 'Valid input';
 }
 
-module.exports = { validateUserInput, validateUsername, validatePassword };
+module.exports = { validateUserInput, validateUsername, validatePassword };function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/[<>]/g, '')
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 8 &&
+           /[A-Z]/.test(password) &&
+           /[a-z]/.test(password) &&
+           /\d/.test(password);
+}
+
+module.exports = {
+    sanitizeInput,
+    validateEmail,
+    validatePassword
+};
