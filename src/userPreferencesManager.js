726b1e332990d80e8230e777e88cd98430985a8b
heyQuim <hey.quim@hotmail.com>
2026-02-28 00:07:54 +0000

    Add user preferences management with localStorage support

--- src/userPreferencesManager.js
+++ src/userPreferencesManager.js
@@ -745,4 +745,50 @@ if (typeof module !== 'undefined' && module.exports) {
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = UserPreferencesManager;
-}
+}const userPreferencesManager = (function() {
+    const PREFERENCES_KEY = 'app_preferences';
+    
+    function getPreferences() {
+        const stored = localStorage.getItem(PREFERENCES_KEY);
+        return stored ? JSON.parse(stored) : {};
+    }
+    
+    function setPreference(key, value) {
+        const preferences = getPreferences();
+        preferences[key] = value;
+        localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+        return true;
+    }
+    
+    function getPreference(key, defaultValue = null) {
+        const preferences = getPreferences();
+        return preferences.hasOwnProperty(key) ? preferences[key] : defaultValue;
+    }
+    
+    function removePreference(key) {
+        const preferences = getPreferences();
+        if (preferences.hasOwnProperty(key)) {
+            delete preferences[key];
+            localStorage.setItem(PREFERENCES_KEY, JSON.stringify(preferences));
+            return true;
+        }
+        return false;
+    }
+    
+    function clearPreferences() {
+        localStorage.removeItem(PREFERENCES_KEY);
+        return true;
+    }
+    
+    function getAllPreferences() {
+        return getPreferences();
+    }
+    
+    return {
+        get: getPreference,
+        set: setPreference,
+        remove: removePreference,
+        clear: clearPreferences,
+        getAll: getAllPreferences
+    };
+})();
