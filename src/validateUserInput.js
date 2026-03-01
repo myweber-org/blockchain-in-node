e23349bb620c17344c3d54ae26d1f7a7b9dc7b99
heyQuim <hey.quim@hotmail.com>
2026-03-01 01:27:09 +0000

    Add input validation function for username and password

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -288,4 +288,29 @@ export { validateUserInput, validateUsername, validatePassword };function valida
         isValid: errors.length === 0,
         errors: errors
     };
+}function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
 }
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid && !passwordValid) {
+        return 'Username and password are invalid';
+    } else if (!usernameValid) {
+        return 'Username is invalid';
+    } else if (!passwordValid) {
+        return 'Password is invalid';
+    }
+    
+    return 'User input is valid';
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
