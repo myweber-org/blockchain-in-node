d13bf4500fe14420ac3c72f021aaae805aaaba0d
InkspillVerse <inkspill.verse@outlook.com>
2026-03-01 01:04:15 +0000

    Add email validation utility function

--- src/ledger/validateEmail.js
+++ src/ledger/validateEmail.js
@@ -37,4 +37,9 @@ module.exports = validateEmail;function validateEmail(email) {
 }function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
+}function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
+
+module.exports = validateEmail;
