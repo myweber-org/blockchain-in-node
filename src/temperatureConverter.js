99387a6852f2784869c18ea6615249ad5318409a
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-03 19:01:50 +0000

    Add utility function to convert Celsius to Fahrenheit

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -135,4 +135,12 @@ export {
     kelvinToFahrenheit,
     convertTemperature,
     formatTemperature
-};
+};function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+export { celsiusToFahrenheit, fahrenheitToCelsius };
