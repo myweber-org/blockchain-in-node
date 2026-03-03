eda3621babf8e6fdd11c1d5735eb8dc3af872557
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-03 22:32:28 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -1,6 +1,6 @@
 function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 }
 
 module.exports = validateEmail;
