ee5439022c094bc261b3f4ff2d2b68aa24782b63
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-04 16:32:17 +0000

    Add function to calculate the area of a circle given its radius

--- src/calculateCircleArea.js
+++ src/calculateCircleArea.js
@@ -2,36 +2,7 @@ function calculateCircleArea(radius) {
     if (typeof radius !== 'number' || radius <= 0) {
         throw new Error('Radius must be a positive number');
     }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
-}function calculateCircleArea(radius) {
-    if (typeof radius !== 'number' || radius < 0) {
-        throw new Error('Radius must be a non-negative number');
-    }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
-}
-
-module.exports = calculateCircleArea;function calculateCircleArea(radius) {
-    if (typeof radius !== 'number' || radius <= 0) {
-        throw new Error('Radius must be a positive number');
-    }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
-}function calculateCircleArea(radius) {
-    if (typeof radius !== 'number' || radius < 0) {
-        throw new Error('Radius must be a non-negative number');
-    }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
-}
-
-module.exports = calculateCircleArea;function calculateCircleArea(radius) {
-    if (typeof radius !== 'number' || radius <= 0) {
-        throw new Error('Radius must be a positive number');
-    }
-    const area = Math.PI * Math.pow(radius, 2);
-    return parseFloat(area.toFixed(2));
+    return Math.PI * radius * radius;
 }
 
 module.exports = calculateCircleArea;
