df0e78b3c18b82e95296dd13e532b87af8cb1eed
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-07 20:18:18 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -1,18 +1,9 @@
 function calculateFibonacci(n) {
     if (n <= 0) return [];
     if (n === 1) return [0];
-    
-    const sequence = [0, 1];
+    let fib = [0, 1];
     for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
+        fib.push(fib[i - 1] + fib[i - 2]);
     }
-    return sequence;
+    return fib;
 }
-
-function displayFibonacci(limit) {
-    const result = calculateFibonacci(limit);
-    console.log(`Fibonacci sequence up to ${limit} numbers:`, result);
-    return result;
-}
-
-export { calculateFibonacci, displayFibonacci };
