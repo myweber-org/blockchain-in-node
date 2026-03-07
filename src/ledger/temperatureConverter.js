9e316743b902fdf317e1e22559c6b6b4d26aecbc
FelixMakes <felix.makes@hotmail.com>
2026-03-07 15:47:00 +0000

    Add utility function for converting between Celsius and Fahrenheit

--- src/ledger/temperatureConverter.js
+++ src/ledger/temperatureConverter.js
@@ -7,9 +7,9 @@ function fahrenheitToCelsius(fahrenheit) {
 }
 
 function convertTemperature(value, unit) {
-    if (unit.toLowerCase() === 'c') {
+    if (unit === 'C') {
         return celsiusToFahrenheit(value);
-    } else if (unit.toLowerCase() === 'f') {
+    } else if (unit === 'F') {
         return fahrenheitToCelsius(value);
     } else {
         throw new Error('Invalid unit. Use "C" for Celsius or "F" for Fahrenheit.');
