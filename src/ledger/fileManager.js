9b5d0026c3f9b3ad83c70c828e9590a73a87fc9f
ElaraWrites <elara.writes@outlook.com>
2026-03-07 15:10:27 +0000

    Add file management utility with create, read, and delete operations

--- src/ledger/fileManager.js
+++ src/ledger/fileManager.js
@@ -307,4 +307,41 @@ class FileManager {
   }
 }
 
-module.exports = FileManager;
+module.exports = FileManager;class FileManager {
+  constructor() {
+    this.files = new Map();
+  }
+
+  createFile(filename, content) {
+    if (this.files.has(filename)) {
+      throw new Error(`File ${filename} already exists`);
+    }
+    this.files.set(filename, content);
+    return { filename, content };
+  }
+
+  readFile(filename) {
+    if (!this.files.has(filename)) {
+      throw new Error(`File ${filename} not found`);
+    }
+    return this.files.get(filename);
+  }
+
+  deleteFile(filename) {
+    if (!this.files.has(filename)) {
+      throw new Error(`File ${filename} not found`);
+    }
+    this.files.delete(filename);
+    return true;
+  }
+
+  listFiles() {
+    return Array.from(this.files.keys());
+  }
+
+  getFileCount() {
+    return this.files.size;
+  }
+}
+
+export default FileManager;
