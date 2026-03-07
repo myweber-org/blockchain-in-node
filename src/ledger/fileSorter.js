f23ee0b3c114fd9733aea5cd2fb6509233248460
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-07 19:05:39 +0000

    Add utility function to sort files by extension and size

--- src/ledger/fileSorter.js
+++ src/ledger/fileSorter.js
@@ -26,4 +26,13 @@ function groupFilesByExtension(files) {
   return grouped;
 }
 
-export { sortFiles, groupFilesByExtension };
+export { sortFiles, groupFilesByExtension };function sortFiles(files) {
+    return files.sort((a, b) => {
+        const extA = a.name.split('.').pop().toLowerCase();
+        const extB = b.name.split('.').pop().toLowerCase();
+        if (extA !== extB) {
+            return extA.localeCompare(extB);
+        }
+        return a.size - b.size;
+    });
+}
