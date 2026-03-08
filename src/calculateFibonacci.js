6cfda32686cfae33365b2c35592b12426218a9f4
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-08 00:29:26 +0000

    Add function to calculate Fibonacci sequence up to n terms

--- src/calculateFibonacci.js
+++ src/calculateFibonacci.js
@@ -6,4 +6,30 @@ function calculateFibonacci(n) {
         fib.push(fib[i - 1] + fib[i - 2]);
     }
     return fib;
+}function calculateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    const sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
 }
+
+function isFibonacciNumber(num) {
+    if (num < 0) return false;
+    if (num === 0 || num === 1) return true;
+
+    let a = 0;
+    let b = 1;
+    while (b < num) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b === num;
+}
+
+module.exports = { calculateFibonacci, isFibonacciNumber };
