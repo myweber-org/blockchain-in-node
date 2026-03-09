87d422c6b2de93ca4eb23fc74c2367a372c2b6a2
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-09 15:08:06 +0000

    Add file encryption utility with AES-256-CBC implementation

--- src/file-encryption-utility.js
+++ src/file-encryption-utility.js
@@ -84,4 +84,58 @@ class FileEncryptionUtility {
     }
 }
 
+module.exports = FileEncryptionUtility;const crypto = require('crypto');
+
+class FileEncryptionUtility {
+    constructor() {
+        this.algorithm = 'aes-256-cbc';
+        this.keyLength = 32;
+        this.ivLength = 16;
+    }
+
+    generateKey() {
+        return crypto.randomBytes(this.keyLength);
+    }
+
+    encryptFile(data, key) {
+        const iv = crypto.randomBytes(this.ivLength);
+        const cipher = crypto.createCipheriv(this.algorithm, key, iv);
+        
+        let encrypted = cipher.update(data, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        
+        return {
+            iv: iv.toString('hex'),
+            encryptedData: encrypted
+        };
+    }
+
+    decryptFile(encryptedData, key, iv) {
+        const decipher = crypto.createDecipheriv(
+            this.algorithm, 
+            key, 
+            Buffer.from(iv, 'hex')
+        );
+        
+        let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+        decrypted += decipher.final('utf8');
+        
+        return decrypted;
+    }
+
+    createHash(data) {
+        return crypto.createHash('sha256').update(data).digest('hex');
+    }
+
+    validateKey(key) {
+        if (!Buffer.isBuffer(key)) {
+            throw new Error('Key must be a Buffer');
+        }
+        if (key.length !== this.keyLength) {
+            throw new Error(`Key must be ${this.keyLength} bytes`);
+        }
+        return true;
+    }
+}
+
 module.exports = FileEncryptionUtility;
