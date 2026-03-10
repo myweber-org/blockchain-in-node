603a9f655dea97f0f4e620ffadb979931f5c8dca
InkspillVerse <inkspill.verse@outlook.com>
2026-03-10 15:40:05 +0000

    Add a utility function to sort files by extension and size

--- src/ledger/fileSorter.js
+++ src/ledger/fileSorter.js
@@ -35,4 +35,12 @@ export { sortFiles, groupFilesByExtension };function sortFiles(files) {
         }
         return a.size - b.size;
     });
+}function sortFiles(files) {
+    return files.sort((a, b) => {
+        const extA = a.name.split('.').pop().toLowerCase();
+        const extB = b.name.split('.').pop().toLowerCase();
+        if (extA < extB) return -1;
+        if (extA > extB) return 1;
+        return a.size - b.size;
+    });
 }
