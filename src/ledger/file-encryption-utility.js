c26b1b746064a38c98aade04fb1396cafaf5c4bd
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-10 19:30:13 +0000

    Add file encryption utility with AES-256-CBC

--- src/ledger/file-encryption-utility.js
+++ src/ledger/file-encryption-utility.js
@@ -189,4 +189,44 @@ class FileEncryption {
     }
 }
 
-module.exports = FileEncryption;
+module.exports = FileEncryption;const crypto = require('crypto');
+
+class FileEncryptionUtility {
+    constructor() {
+        this.algorithm = 'aes-256-cbc';
+        this.keyLength = 32;
+        this.ivLength = 16;
+    }
+
+    generateKey() {
+        return crypto.randomBytes(this.keyLength);
+    }
+
+    generateIV() {
+        return crypto.randomBytes(this.ivLength);
+    }
+
+    encryptFile(data, key, iv) {
+        const cipher = crypto.createCipheriv(this.algorithm, key, iv);
+        let encrypted = cipher.update(data, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        return {
+            encryptedData: encrypted,
+            iv: iv.toString('hex')
+        };
+    }
+
+    decryptFile(encryptedData, key, ivHex) {
+        const iv = Buffer.from(ivHex, 'hex');
+        const decipher = crypto.createDecipheriv(this.algorithm, key, iv);
+        let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+        decrypted += decipher.final('utf8');
+        return decrypted;
+    }
+
+    createKeyFromPassword(password, salt) {
+        return crypto.pbkdf2Sync(password, salt, 100000, this.keyLength, 'sha256');
+    }
+}
+
+module.exports = FileEncryptionUtility;
