f2b8e88737649082f4a9a0fab9ceb7d8af5dd5ad
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-10 19:46:13 +0000

    Add random color generator utility function

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -68,4 +68,21 @@ function generateRandomHslColor() {
   return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
-export { generateRandomColor, generateRandomRgbColor, generateRandomHslColor };
+export { generateRandomColor, generateRandomRgbColor, generateRandomHslColor };function generateRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomColorArray(count) {
+  const colors = [];
+  for (let i = 0; i < count; i++) {
+    colors.push(generateRandomColor());
+  }
+  return colors;
+}
+
+export { generateRandomColor, getRandomColorArray };
