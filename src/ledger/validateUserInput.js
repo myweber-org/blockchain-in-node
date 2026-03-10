5d874fbfb4017cd47ba8e6598a7200df8857e7fa
heyQuim <hey.quim@hotmail.com>
2026-03-10 23:12:09 +0000

    Add input validation function to sanitize user-provided strings

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -70,4 +70,25 @@ function validateUserInput(username, password) {
     };
 }
 
-export { validateUserInput, validateUsername, validatePassword };
+export { validateUserInput, validateUsername, validatePassword };function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input.trim()
+        .replace(/[<>]/g, '')
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 8 &&
+           /[A-Z]/.test(password) &&
+           /[a-z]/.test(password) &&
+           /\d/.test(password);
+}
+
+module.exports = { sanitizeInput, validateEmail, validatePassword };
