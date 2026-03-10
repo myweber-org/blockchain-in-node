e7888e662461a9ed0c39c3ed6999325356b2c37b
FelixMakes <felix.makes@hotmail.com>
2026-03-10 22:19:31 +0000

    Add function to calculate monthly mortgage payment with principal, interest rate, and loan term

--- src/ledger/calculateMonthlyPayment.js
+++ src/ledger/calculateMonthlyPayment.js
@@ -107,4 +107,17 @@ module.exports = {
     calculateMonthlyPayment,
     formatCurrency,
     validateLoanInput
-};
+};function calculateMonthlyPayment(principal, annualInterestRate, loanTermYears) {
+    const monthlyInterestRate = annualInterestRate / 12 / 100;
+    const totalPayments = loanTermYears * 12;
+    
+    if (monthlyInterestRate === 0) {
+        return principal / totalPayments;
+    }
+    
+    const monthlyPayment = principal * 
+        (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments)) / 
+        (Math.pow(1 + monthlyInterestRate, totalPayments) - 1);
+    
+    return Math.round(monthlyPayment * 100) / 100;
+}
