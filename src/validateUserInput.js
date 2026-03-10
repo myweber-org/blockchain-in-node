63e1b13c3e1d8694aaee02c0481d123e01023905
FelixMakes <felix.makes@hotmail.com>
2026-03-10 15:35:51 +0000

    Add input validation function to sanitize user-provided strings

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -91,4 +91,28 @@ module.exports = { validateUserInput, validateUsername, validatePassword };funct
     };
 }
 
-module.exports = validateUserInput;
+module.exports = validateUserInput;function sanitizeInput(input) {
+    if (typeof input !== 'string') {
+        return '';
+    }
+    return input
+        .replace(/[<>&"']/g, function(match) {
+            const escapeMap = {
+                '<': '&lt;',
+                '>': '&gt;',
+                '&': '&amp;',
+                '"': '&quot;',
+                "'": '&#x27;'
+            };
+            return escapeMap[match] || match;
+        })
+        .trim()
+        .substring(0, 255);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+module.exports = { sanitizeInput, validateEmail };
