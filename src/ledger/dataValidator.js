e623074e18f133cf9b0e05fe00261756837184b1
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-11 20:32:10 +0000

    Add data validation utility for user input sanitization

--- src/ledger/dataValidator.js
+++ src/ledger/dataValidator.js
@@ -29,4 +29,24 @@ function validateFormData(data) {
     };
 }
 
-export { validateEmail, validatePhoneNumber, validateFormData };
+export { validateEmail, validatePhoneNumber, validateFormData };function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    return password.length >= 8 && 
+           /[A-Z]/.test(password) && 
+           /[a-z]/.test(password) && 
+           /\d/.test(password);
+}
+
+function sanitizeInput(input) {
+    return input.replace(/[<>]/g, '');
+}
+
+module.exports = {
+    validateEmail,
+    validatePassword,
+    sanitizeInput
+};
