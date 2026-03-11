0e29bf692feae25697a2b6a718ab9402169e363a
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-11 16:33:04 +0000

    Add function to compute the nth Fibonacci number using memoization

--- src/ledger/calculateFibonacci.js
+++ src/ledger/calculateFibonacci.js
@@ -1,12 +1,8 @@
-function calculateFibonacci(limit) {
-    if (limit <= 0) return [];
-    if (limit === 1) return [0];
-    
-    const sequence = [0, 1];
-    while (true) {
-        const nextValue = sequence[sequence.length - 1] + sequence[sequence.length - 2];
-        if (nextValue > limit) break;
-        sequence.push(nextValue);
-    }
-    return sequence;
+function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
+
+module.exports = fibonacci;
