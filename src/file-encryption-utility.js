ba5d1fa079621e5e6f2c05f347eaca5b11923b29
heyQuim <hey.quim@hotmail.com>
2026-03-12 14:17:12 +0000

    Add file encryption utility with AES-256-CBC

--- src/file-encryption-utility.js
+++ src/file-encryption-utility.js
@@ -138,4 +138,73 @@ class FileEncryptionUtility {
     }
 }
 
-module.exports = FileEncryptionUtility;
+module.exports = FileEncryptionUtility;const crypto = require('crypto');
+const fs = require('fs');
+
+class FileEncryptor {
+    constructor(key) {
+        this.algorithm = 'aes-256-cbc';
+        this.key = crypto.createHash('sha256').update(key).digest();
+    }
+
+    encryptFile(inputPath, outputPath) {
+        return new Promise((resolve, reject) => {
+            try {
+                const iv = crypto.randomBytes(16);
+                const cipher = crypto.createCipheriv(this.algorithm, this.key, iv);
+                
+                const input = fs.createReadStream(inputPath);
+                const output = fs.createWriteStream(outputPath);
+                
+                output.write(iv);
+                
+                input.pipe(cipher).pipe(output);
+                
+                output.on('finish', () => {
+                    resolve('Encryption completed successfully');
+                });
+                
+                output.on('error', reject);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    decryptFile(inputPath, outputPath) {
+        return new Promise((resolve, reject) => {
+            try {
+                const input = fs.createReadStream(inputPath, { start: 0, end: 15 });
+                let iv;
+                
+                input.on('data', (data) => {
+                    iv = data;
+                });
+                
+                input.on('end', () => {
+                    const decipher = crypto.createDecipheriv(this.algorithm, this.key, iv);
+                    const fileInput = fs.createReadStream(inputPath, { start: 16 });
+                    const fileOutput = fs.createWriteStream(outputPath);
+                    
+                    fileInput.pipe(decipher).pipe(fileOutput);
+                    
+                    fileOutput.on('finish', () => {
+                        resolve('Decryption completed successfully');
+                    });
+                    
+                    fileOutput.on('error', reject);
+                });
+                
+                input.on('error', reject);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    static generateRandomKey(length = 32) {
+        return crypto.randomBytes(length).toString('hex');
+    }
+}
+
+module.exports = FileEncryptor;
