42048540e40bd8dfcbbd54587486089de8ce7bdb
InkspillVerse <inkspill.verse@outlook.com>
2026-03-12 14:25:51 +0000

    Add random color generator utility function

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -109,3 +109,21 @@ function generateRandomRGBA(alpha = Math.random().toFixed(2)) {
 }
 
 export { generateRandomColor, generateRandomRGB, generateRandomRGBA };
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function generateRandomColors(count) {
+  const colors = [];
+  for (let i = 0; i < count; i++) {
+    colors.push(getRandomColor());
+  }
+  return colors;
+}
+
+export { getRandomColor, generateRandomColors };
