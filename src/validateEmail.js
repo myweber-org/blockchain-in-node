3a1de91c352ba0fda73002180e96e0b7e929c829
heyQuim <hey.quim@hotmail.com>
2026-03-12 00:43:05 +0000

    Add email validation utility function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -49,4 +49,9 @@ module.exports = validateEmail;function validateEmail(email) {
 }function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
+}function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
+
+module.exports = validateEmail;
