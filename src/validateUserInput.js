a0d5689164b7701641c269e8b6c499520ccf9aac
VioletInquiry <violet.inquiry@outlook.com>
2026-03-12 21:53:49 +0000

    Add input validation for username and password fields

--- src/validateUserInput.js
+++ src/validateUserInput.js
@@ -362,4 +362,24 @@ function validateUserInput(username, password) {
     return null;
 }
 
+module.exports = { validateUserInput, validateUsername, validatePassword };function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    if (!validateUsername(username)) {
+        throw new Error('Invalid username format');
+    }
+    if (!validatePassword(password)) {
+        throw new Error('Password does not meet security requirements');
+    }
+    return true;
+}
+
 module.exports = { validateUserInput, validateUsername, validatePassword };
