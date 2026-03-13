0611e2eb4afc23206704e78c4cd21cc07f2c5a32
heyQuim <hey.quim@hotmail.com>
2026-03-13 15:26:17 +0000

    Add input validation for username and password fields

--- src/ledger/validateUserInput.js
+++ src/ledger/validateUserInput.js
@@ -98,4 +98,29 @@ module.exports = validateUserInput;function validateUserInput(username, email) {
     }
 
     return { username, email };
+}function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+    return usernameRegex.test(username);
 }
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateUserInput(username, password) {
+    const usernameValid = validateUsername(username);
+    const passwordValid = validatePassword(password);
+    
+    if (!usernameValid) {
+        throw new Error('Invalid username format');
+    }
+    
+    if (!passwordValid) {
+        throw new Error('Password must be at least 8 characters with uppercase, lowercase, number and special character');
+    }
+    
+    return true;
+}
+
+module.exports = { validateUserInput, validateUsername, validatePassword };
